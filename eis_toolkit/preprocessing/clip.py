95f4446ebe089d94e3662382cfba3c66ccabbb16
pavetsu14 <55135974+pavetsu14@users.noreply.github.com>
2022-07-21 11:45:56 +0300

    Improve clip (#24)
    Update dependencies and improve clip functionality

--- eis_toolkit/preprocessing/clip.py
+++ eis_toolkit/preprocessing/clip.py
@@ -1,30 +1,35 @@
-import numpy as np
 import geopandas as gpd
 from pathlib import Path
 import rasterio
 from rasterio.mask import mask
-from affine import Affine
-from typing import Tuple
 from eis_toolkit.exceptions import NonMatchingCrsException, NotApplicableGeometryTypeException
 
 
-def clip_ras(rasin: Path, polin: Path) -> Tuple[np.ndarray, Affine]:
-    """Clips raster with polygon.
+def clip_ras(rasin: Path, polin: Path, rasout: Path) -> None:
+    """Clips raster with polygon and saves resulting raster into given folder location.
 
     Args:
         rasin (Path): file path to input raster
         polin (Path): file path to polygon to be used for clipping the input raster
-
-    Returns:
-        Tuple[np.ndarray, Affine]: tuple consisting of clipped raster in array format and georeferencing information
+        rasout (Path): file path to output raster
     """
     pol_df = gpd.read_file(polin)
+
     with rasterio.open(rasin) as src:
         pol_geom = pol_df['geometry'][0]
         if pol_geom.geom_type not in ['Polygon', 'MultiPolygon']:
-            raise(NotApplicableGeometryTypeException)
+            raise (NotApplicableGeometryTypeException)
         if src.crs != pol_df.crs:
-            raise(NonMatchingCrsException)
+            raise (NonMatchingCrsException)
         out_image, out_transform = mask(src, [pol_geom], crop=True)
+        out_meta = src.meta
 
-    return out_image, out_transform
+    out_meta.update({'driver': 'GTiff',
+                     'height': out_image.shape[1],
+                     'width': out_image.shape[2],
+                     'transform': out_transform})
+
+    with rasterio.open(rasout, 'w', **out_meta) as dest:
+        dest.write(out_image)
+
+    return
--- tests/clip_test.py
+++ tests/clip_test.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import numpy as np
+from pathlib import Path
+import rasterio
 from eis_toolkit.preprocessing.clip import clip_ras
 import pytest
 from eis_toolkit.exceptions import NonMatchingCrsException, NotApplicableGeometryTypeException
@@ -7,30 +8,35 @@ from eis_toolkit.exceptions import NonMatchingCrsException, NotApplicableGeometr
 
 def test_clip():
     """Tests clip functionality with geotiff raster and shapefile polygon."""
-    input_path = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif'
-    pol_path = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_area.shp'
+    input_path = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif')
+    pol_path = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_area.shp')
+    output_path = Path('/home/pauliina/Downloads/eis_outputs/clip_result.tif')
 
-    result = clip_ras(Path(input_path), pol_path)
+    clip_ras(input_path, pol_path, output_path)
 
-    assert np.amax(result[0]) != np.amin(result[0])
-    assert result[0].shape[0] == 1
+    result = rasterio.open(output_path)
+
+    assert np.amax(result.read()) != np.amin(result.read())
+    assert result.count == 1
     # Corresponding operation executed with QGIS for reference solution
-    assert result[1][2] == 384760.0
-    assert result[1][5] == 6671364.0
-    print(result)
+    assert result.height == result.width == 26
+    assert result.bounds[0] == 384760.0
+    assert result.bounds[3] == 6671364.0
 
 
 def test_clip_wrong_geometry_type():
     """Checks that trying to clip a raster file with non-polygon shape returns custom exception error."""
-    input_path = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif'
-    pol_path = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/point.gpkg'
+    input_path = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif')
+    pol_path = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/point.gpkg')
+    output_path = Path('/home/pauliina/Downloads/eis_outputs/clip_result.tif')
     with pytest.raises(NotApplicableGeometryTypeException):
-        clip_ras(Path(input_path), pol_path)
+        clip_ras(input_path, pol_path, output_path)
 
 
 def test_clip_different_crs():
     """Checks that trying to clip a raster file with polygon without matching crs information returns custom exception error."""
-    input_path = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif'
-    pol_df = '/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_area.geojson'
+    input_path = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_raster.tif')
+    pol_df = Path('/home/pauliina/PycharmProjects/eis_toolkit/tests/data/small_area.geojson')
+    output_path = Path('/home/pauliina/Downloads/eis_outputs/clip_result.tif')
     with pytest.raises(NonMatchingCrsException):
-        clip_ras(Path(input_path), pol_df)
+        clip_ras(input_path, pol_df, output_path)
