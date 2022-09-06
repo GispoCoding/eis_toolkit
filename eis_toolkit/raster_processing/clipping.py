f0530c63bdafc3e63f8048d18e8fe2970bdcdaeb
pavetsu14 <pauliina@gispo.fi>
2022-09-06 11:43:28 +0300

    Code style changes

--- eis_toolkit/raster_processing/clipping.py
+++ eis_toolkit/raster_processing/clipping.py
@@ -1,54 +1,46 @@
+from typing import Tuple
+
 import geopandas
+import numpy
 import rasterio
 from rasterio.mask import mask
 
 from eis_toolkit.checks.crs import matching_crs
 from eis_toolkit.checks.geometry import correct_geometry_types
-from eis_toolkit.exceptions import NotApplicableGeometryTypeException
-from eis_toolkit.exceptions import NonMatchingCrsException
+from eis_toolkit.exceptions import NonMatchingCrsException, NotApplicableGeometryTypeException
 
 
-def clip(
+def clip(  # type: ignore[no-any-unimported] # noqa: E261,E262
     raster: rasterio.io.DatasetReader,
     polygon: geopandas.GeoDataFrame,
-):
+) -> Tuple[numpy.ndarray, dict]:
     """Clips a raster with vector geometry (polygon / polygons).
 
     Args:
         raster (rasterio.io.DatasetReader): The raster to be clipped.
-        polygon (geopandas.GeoDataFrame): A geodataframe containing the
-            polygon(s) to do the clipping with.
+        polygon (geopandas.GeoDataFrame): A geodataframe containing the polygon(s) to do the clipping with.
 
     Returns:
-        out_image (numpy.ndarray): The clipped raster data
-        out_meta (dict): The updated metadata
+        out_image (numpy.ndarray): The clipped raster data.
+        out_meta (dict): The updated metadata.
 
     Raises:
-        NonMatchingCrsException: The raster and polygons are not in the same
-            crs
-        NotApplicableGeometryTypeException: The input geometries contain
-            non-polygon features
+        NonMatchingCrsException: The raster and polygons are not in the same crs.
+        NotApplicableGeometryTypeException: The input geometries contain non-polygon features.
     """
 
     shapes = polygon["geometry"]
 
     if not matching_crs([raster, shapes]):
         raise NonMatchingCrsException
+
     if not correct_geometry_types(shapes, allowed=["Polygon", "MultiPolygon"]):
         raise NotApplicableGeometryTypeException
 
-    out_image, out_transform = mask(
-        dataset=raster,
-        shapes=shapes,
-        crop=True,
-        all_touched=True
-    )
+    out_image, out_transform = mask(dataset=raster, shapes=shapes, crop=True, all_touched=True)
     out_meta = raster.meta.copy()
-    out_meta.update({
-        'driver': 'GTiff',
-        'height': out_image.shape[1],
-        'width': out_image.shape[2],
-        'transform': out_transform
-    })
+    out_meta.update(
+        {"driver": "GTiff", "height": out_image.shape[1], "width": out_image.shape[2], "transform": out_transform}
+    )
 
     return out_image, out_meta
