84d197e1ad55183c8c39e78e4aed1e3bb47e9791
Juho Laitala <juho.laitala@gtk.fi>
2022-09-27 09:27:35 +0300

    updated raster_to_pandas

--- eis_toolkit/conversions/raster_to_pandas.py
+++ eis_toolkit/conversions/raster_to_pandas.py
@@ -1,50 +1,85 @@
-from typing import List, Union
-import rasterio
-import pandas as pd
+from typing import List, Optional, Union
+
 import numpy as np
-from typing import List
+import pandas as pd
+import rasterio
 
 
-def raster_to_pandas(
+def _raster_to_pandas(  # type: ignore[no-any-unimported]
     raster: rasterio.io.DatasetReader,
-    bands: Union[int, List[int]] = None,
-    names: List[str] = None,
+    bands: Optional[Union[int, List[int]]] = None,
+    names: Optional[List[str]] = None,
     add_img_coord: bool = False,
 ) -> pd.DataFrame:
 
     if bands is not None:
-        arr = raster.read(bands)
+        data_array = raster.read(bands)
     else:
-        arr = raster.read()
+        data_array = raster.read()
 
-    # Check for existing band names
     if names is None:
-        if not all(rast.descriptions):
+        if not all(raster.descriptions):
             names = ["band_" + str(i) for i in range(1, raster.count + 1)]
         else:
             names = raster.descriptions
 
-    row, col = np.where(np.full(arr.shape[1:], True))  # Image coords for pixels
-    pixel_data = arr[..., row, col].T  # Read data from image coords
+    row, col = np.where(np.full(data_array.shape[1:], True))
+    pixel_data = data_array[..., row, col].T
 
-    if add_img_coord == True:
+    if add_img_coord is True:
         data_with_coord = np.column_stack((pixel_data, np.column_stack((row, col))))
-        df = pd.DataFrame(data_with_coord, columns=names + ["row", "col"])
+        data_frame = pd.DataFrame(data_with_coord, columns=names + ["row", "col"])
     else:
-        df = pd.DataFrame(pixel_data, columns=names)
+        data_frame = pd.DataFrame(pixel_data, columns=names)
 
-    return df
+    return data_frame
+
+
+def raster_to_pandas(  # type: ignore[no-any-unimported]
+    raster: rasterio.io.DatasetReader,
+    bands: Optional[Union[int, List[int]]] = None,
+    band_names: Optional[List[str]] = None,
+    add_img_coord: bool = False,
+) -> pd.DataFrame:
+    """Convert raster to pandas DataFrame.
+
+    If bands are not given, all bands are used for conversion. If band names are not given, band names are read from
+    raster meta data. If raster meta data doesn't contain names, then bands are named as band_1, band_2,...,band_n.
+    If wanted, image coordinates (row, col) for each pixel can be written to dataframe by setting add_img_coord to True.
+
+    Args:
+        raster (rasterio.io.DatasetReader):: Raster to be converted.
+        bands (Union[int, List[int]], optional): Selected band(s) from multiband raster. Can be single band index or
+        list of band indices. Defaults to None.
+        band_names (List[str], optional): Band names. Defaults to None.
+        add_img_coord (bool): Determines if pixel coordinates are written into dataframe. Defaults to false.
+
+    Returns:
+        pd.DataFrame: Raster converted to pandas dataframe
+    """
+    if bands is not None:
+        if not isinstance(bands, int):
+            if not all(isinstance(band, int) for band in bands):
+                raise InvalidBandValue
+
+    data_frame = _raster_to_pandas(
+        raster=raster,
+        bands=bands,
+        names=band_names,
+        add_img_coord=add_img_coord,
+    )
+    return data_frame
 
 
 rast = rasterio.open("tests/data/remote/small_raster.tif")
-rast_arr = rast.read(1)
+rast_data_array = rast.read(1)
 
 multiband = "tests/data/local/data/multiband.tif"
 meta = rast.meta.copy()
 meta["count"] = 4
 with rasterio.open(multiband, "w", **meta) as dest:
     for band in range(1, 5):
-        dest.write(rast_arr - band, band)
+        dest.write(rast_data_array - band, band)
         dest.set_band_description(band, "band_" + str(band))
 
 multi = rasterio.open("tests/data/local/data/multiband.tif")
