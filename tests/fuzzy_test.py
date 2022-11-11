2b23dc81dbe7e0e821e2bfcdd6fe3609ec75c8bf
Johanna Torppa <johanna.torppa@gtk.fi>
2022-11-11 14:49:52 +0200

    Bugfix :remove redundant erroneuous rows

--- tests/fuzzy_test.py
+++ tests/fuzzy_test.py
@@ -4,8 +4,7 @@ from eis_toolkit.modeling import fuzzy_overlay
 from eis_toolkit.exceptions import InvalidParameterValueException
 input_data_with_error = numpy.array([[1,-0.5,0.2],[0.9,0.55,0.15],[0.98,0.45,0.3]])
 input_data_without_error = numpy.array([[1,0.5,0.2],[0.9,0.55,0.15],[0.98,0.45,0.3]])
-input_data_without_error = numpy.array([[1,0.5,NA],[0.9,0.55,0.15],[0.98,0.45,0.3]])
-gam_with_error=[-1,2,NA]
+gam_with_error=-1
 gam_without_error=1
 
 def test_fuzzy_overlay_or_invalid_parameter_data():
@@ -26,4 +25,3 @@ def test_fuzzy_overlay_gamma_invalid_parameter_data():
 def test_fuzzy_overlay_gamma_invalid_parameter_gamma():
     with pytest.raises(InvalidParameterValueException):
         fuzzy_overlay.fuzzy_gamma(input_data_without_error,gam_with_error)
-def 
