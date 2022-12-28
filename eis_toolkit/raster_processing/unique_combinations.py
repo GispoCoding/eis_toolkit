31930a80c86416596f1cb93d8b95b2c5063f41a6
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2022-12-28 12:28:19 +0200

    finalize algorithm, write tests accordingly

--- eis_toolkit/raster_processing/unique_combinations.py
+++ eis_toolkit/raster_processing/unique_combinations.py
@@ -8,36 +8,43 @@ from eis_toolkit.exceptions import InvalidParameterValueException
 
 
 def _unique_combinations(  # type: ignore[no-any-unimported]
-    rasters: List[rasterio.io.DatasetReader],
+    list_of_rasters: List[rasterio.io.DatasetReader],
     path_to_output_file: str
 ) -> rasterio.io.DatasetReader:
 
-    arrays = []
-    for raster in rasters:
-        arrays.append(raster.read())
+    list_of_rasters = sorted(list_of_rasters, key=sort_by_raster_band_count)
+    rasters = []
+    for raster in list_of_rasters:
+        for index in range(0, raster.count):
+            band = index + 1
+            rasters.append(raster.read(band))
 
-    output_array = np.empty_like(arrays[0])
-
+    output_array = np.empty_like(rasters[0])
     combinations: Dict[tuple, int] = {}
-    for band, row, column in itertools.product(range(len(arrays[0])), range(len(arrays[0][0])), range(len(arrays[0][0][0]))):
-        combination = tuple(arrays[i][band][row][column] for i, _ in enumerate(arrays))
+    for row, column in itertools.product(range(len(rasters[0])), range(len(rasters[0][0]))):
+
+        combination = tuple(rasters[raster][row][column] for raster, _ in enumerate(rasters))
+
         if combination not in combinations:
             combinations[combination] = len(combinations) + 1
-        output_array[0][row][column] = combinations[combination]
+        output_array[row][column] = combinations[combination]
 
-    with rasterio.open(path_to_output_file, 'w', **rasters[0].meta) as write_raster:
-        write_raster.write(output_array)
+    with rasterio.open(path_to_output_file, 'w', **list_of_rasters[0].meta) as write_raster:
+        write_raster.write(output_array, 1)
     output_raster = rasterio.open(path_to_output_file)
     return output_raster
 
+def sort_by_raster_band_count(raster):
+    return raster.count
+
 
 def unique_combinations(  # type: ignore[no-any-unimported]
-    rasters: List[rasterio.io.DatasetReader],
+    list_of_rasters: List[rasterio.io.DatasetReader],
     path_to_output_file: str
 ) -> rasterio.io.DatasetReader:
     """Get combinations of raster values between rasters.
 
-    All bands are used for analysis.
+    All bands in all rasters are used for analysis.
 
     Args:
         rasters (List[rasterio.io.DatasetReader]): Rasters to be used for finding combinations.
@@ -46,18 +53,21 @@ def unique_combinations(  # type: ignore[no-any-unimported]
     Returns:
         rasterio.io.DatasetReader: Raster with unique combinations
     """
-    arrays = []
-    for raster in rasters:
-        arrays.append(raster.read())
+    rasters = []
+    for raster in list_of_rasters:
+        rasters.append(raster.read())
 
-    row_length = len(arrays[0][0])
-    column_length = len(arrays[0][0][0])
+    row_length = len(rasters[0][0])
+    column_length = len(rasters[0][0][0])
 
-    if not all(len(array[0]) == row_length for array in arrays):
+    if len(list_of_rasters) == 1:
         raise InvalidParameterValueException
 
-    if not all(len(array[0][0]) == column_length for array in arrays):
+    if not all(len(raster[0]) == row_length for raster in rasters):
         raise InvalidParameterValueException
 
-    output_raster = _unique_combinations(rasters, path_to_output_file)
+    if not all(len(raster[0][0]) == column_length for raster in rasters):
+        raise InvalidParameterValueException
+
+    output_raster = _unique_combinations(list_of_rasters, path_to_output_file)
     return output_raster
--- tests/unique_combinations_test.py
+++ tests/unique_combinations_test.py
@@ -11,14 +11,27 @@ from eis_toolkit.exceptions import InvalidParameterValueException
 parent_dir = Path(__file__).parent
 raster_path = parent_dir.joinpath("data/remote/small_raster.tif")
 output_raster_path = parent_dir.joinpath("data/remote/small_raster - Copy.tif")
+expected_1st_row = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0,
+19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0,
+37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0]
+expected_2nd_row = [47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0,
+63.0, 64.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0, 81.0, 82.0,
+83.0, 84.0, 85.0, 86.0, 87.0, 88.0, 82.0, 89.0, 90.0, 91.0]
 
 def test_unique_combinations():
-    """Test unique combinations."""
-    src = rasterio.open(raster_path)
-    src_2 = rasterio.open(raster_path)
+    """Test unique combinations creates expected output."""
+    raster_1 = rasterio.open(raster_path)
+    raster_2 = rasterio.open(raster_path)
 
-    unique_combinations_raster = unique_combinations([src, src_2], output_raster_path)
+    unique_combinations_raster = unique_combinations([raster_1, raster_2], output_raster_path)
+    
+    assert unique_combinations_raster.meta == raster_1.meta
+    assert unique_combinations_raster.read(1)[0].tolist() == expected_1st_row
+    assert unique_combinations_raster.read(1)[1].tolist() == expected_2nd_row
 
-    assert len(src.read(1)) == len(unique_combinations_raster.read(1))
-    assert src.read(1) is not unique_combinations_raster.read(1)
 
+def test_unique_combinations():
+    """Test that invalid parameter values for rasters raises correct exception."""
+    with pytest.raises(InvalidParameterValueException):
+        with rasterio.open(raster_path) as raster:
+            unique_combinations([raster], output_raster_path)
