b4580cafb8da199a56c1c78dd862d548ee1348bd
dipak6697 <nidhi73dipak@gmail.com>
2023-02-14 12:04:23 +0200

    pull request is ok

--- tests/PCA.py
+++ tests/PCA.py
@@ -5,56 +5,36 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-# Reading the data
-
-num_components = 2
-#importing 1112X9 data frame
-path1 = "1112x9.csv"
-#importing 17X9 data frame
-path2 = "17x9.csv"
-df1 = pd.read_csv(path1)
-df2 = pd.read_csv(path2)
-
 #defining the function for PCA
+def Compute_PCA(data_to_compute, num_components, show_results=False):
+    """THIS FUNCTOIN TAKES DATAFRAME AS INPUT, CALCULATES THE PRINCIPLE COMPONENTS AND RETURS A DATAFRAME CONTAINING THE PRINCIPAL COMPONETS INSIDE IT"""
+    try:
+        # Separating out the features
+        data = data_to_compute.loc[:, data_to_compute.columns].values
 
-def MyPCA(X):
-    # standardize the Data
-    features1 = X.columns
+        # Standardizing the features
+        data = StandardScaler().fit_transform(data)
 
-    # Separating out the features
-    x1 = X.loc[:, features1].values
+        # Calculating the PCA
+        pca = PCA(n_components=num_components)
+        principalComponent = pca.fit_transform(data)
 
-    # Standardizing the features
-    x1 = StandardScaler().fit_transform(x1)
+        columns_name_holder = []
+        for i in range(num_components):
+            columns_name_holder.append(f'principal_component_{i+1}')
 
-    # Calculating the PCA
-    pca = PCA(n_components=num_components)
-    principalComponents1 = pca.fit_transform(x1)
-    dataframe = pd.DataFrame(data=principalComponents1, columns=['principal_component_1', 'principal_component_2'])
+        # create a data frame with ocs inside 
+        dataframe = pd.DataFrame(data=principalComponent, columns=columns_name_holder)
 
-    return dataframe
+        # if show the res is true print it on screen 
+        if(show_results):
+            print(dataframe)
+
+        return dataframe
+    except Exception as e:
+        print(f"[EXCEPTION] Compute the pca throws exception {e}")
+        return None
 
 
-# Defining the function for plotting the PCA
 
-def PCA_plot(X1, X2):
-    #plotting the graph
-    plt.scatter(X2.principal_component_1, X2.principal_component_2,marker='.', color='blue', s=200, alpha = .2)
-    plt.scatter(X1.principal_component_1, X1.principal_component_2,marker='*', color='red', s=500)
-    matplotlib.rcParams.update({'font.size': 22})
-    plt.title("Principal components of all points")
-    plt.xlabel('Principal component 1')
-    plt.ylabel('Principal component 2')
-
-    plt.show()
     
-    
-# Calling the function for PCA
-
-principal_df1 = MyPCA(X=df1)
-principal_df2 = MyPCA(X=df2)
-
-#calling the function for scatter plot
-
-PCA_plot(principal_df2,principal_df1)
