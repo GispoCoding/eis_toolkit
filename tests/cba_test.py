6ffd7804bf69f296d3288b10bbe794862d54ecf0
AlVella <125969544+AlVella@users.noreply.github.com>
2023-05-24 10:09:58 +0200

    Update cba_test.py

--- tests/cba_test.py
+++ tests/cba_test.py
@@ -1,6 +1,4 @@
-import warnings
 from pathlib import Path
-
 import geopandas as gpd
 
 from eis_toolkit.spatial_analyses.cba import CBA
@@ -11,14 +9,16 @@ points_path = str(parent_dir.joinpath("data/remote/Test_Occ.shp"))
 lines_path = str(parent_dir.joinpath("data/remote/Test_Faults.shp"))
 matrix_path = str(parent_dir.joinpath("data/remote/Test_CBA_matrix.shp"))
 
+vector_file = gpd.GeoDataFrame.from_file(vector_file_path)
+points_file = gpd.GeoDataFrame.from_file(points_path)
+lines_file = gpd.GeoDataFrame.from_file(lines_path)
+
 
 def test_crs():
     """Test that Coordinate Systems for input dataset and output grids are equivalent."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
-    vector_file = gpd.GeoDataFrame.from_file(vector_path)
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     shp_cba_grid = CBA()
     shp_cba_grid = shp_cba_grid.from_vector_file(matrix_path)
@@ -28,10 +28,9 @@ def test_crs():
 
 def test_gridding():
     """Test that cells indexing is coherent."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     shp_cba_grid = CBA()
     shp_cba_grid = shp_cba_grid.from_vector_file(matrix_path)
@@ -40,12 +39,10 @@ def test_gridding():
 
 def test_code_envs():
     """Test that binary code produced are coherent."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
-    vector_file = gpd.GeoDataFrame.from_file(vector_path)
     names = list(vector_file.Litho.unique())
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     shp_cba_grid = CBA()
     shp_cba_grid = shp_cba_grid.from_vector_file(matrix_path)
@@ -54,14 +51,13 @@ def test_code_envs():
 
 def test_add_points():
     """Test the add_layer() function for points vector file."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     cba_grid.add_layer(
-        vector_file_path=points_path,
-        target_attribut="",
+        geodataframe=points_file,
+        column="",
         subset_of_target_attribut_values=None,
         Name="Occ",
         buffer=False,
@@ -73,13 +69,12 @@ def test_add_points():
 
 def test_add_points_buffered():
     """Test the add_layer() function for points vector file with buffer option."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     cba_grid.add_layer(
-        vector_file_path=points_path, target_attribut="", subset_of_target_attribut_values=None, Name="Occ", buffer=4000
+        geodataframe=points_path, column="", subset_of_target_attribut_values=None, Name="Occ", buffer=4000
     )
     shp_cba_grid = CBA()
     shp_cba_grid = shp_cba_grid.from_vector_file(matrix_path)
@@ -88,13 +83,12 @@ def test_add_points_buffered():
 
 def test_add_lines():
     """Test the add_layer() function for mutltilines vector file."""
-    warnings.simplefilter(action="ignore", category=FutureWarning)
     cba_grid = CBA()
     cba_grid.init_from_vector_file(
-        cell_size=4000, vector_file_path=vector_path, target_attribut="Litho", subset_of_target_attribut_values="all"
+        cell_size=4000, geodataframe=vector_file, column="Litho", subset_of_target_attribut_values="all"
     )
     cba_grid.add_layer(
-        vector_file_path=lines_path,
+        geodataframe=lines_file,
         target_attribut="Type",
         subset_of_target_attribut_values=["Thrust", "Normal"],
         Name=None,
