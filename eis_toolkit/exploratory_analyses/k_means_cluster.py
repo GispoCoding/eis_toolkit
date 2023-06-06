fb24dbe1ad57aceb5725eff61f7543d226dbbe3b
msorvoja <mika@gispo.fi>
2023-06-06 10:05:39 +0300

    Add function for computing k-means clustering and add a test function

---
+++ eis_toolkit/exploratory_analyses/k_means_cluster.py
@@ -0,0 +1,48 @@
+import geopandas as gdp
+import numpy as np
+from beartype import beartype
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+from eis_toolkit.exceptions import EmptyDataFrameException
+
+
+def _k_means_clustering(
+    data: gdp.GeoDataFrame, number_of_clusters: int, random_state: int | None = None
+) -> gdp.GeoDataFrame:
+
+    coordinates = data.geometry.apply(lambda geom: [geom.x, geom.y])
+    coordinates_array = np.array(coordinates.tolist())
+    standardized_data = StandardScaler().fit_transform(coordinates_array)
+
+    kmeans = KMeans(n_clusters=number_of_clusters, random_state=random_state)
+    kmeans.fit(standardized_data)
+
+    data["cluster"] = kmeans.labels_
+
+    return data
+
+
+@beartype
+def k_means_clustering(
+    data: gdp.GeoDataFrame, number_of_clusters: int, random_state: int | None = None
+) -> gdp.GeoDataFrame:
+    """
+    Perform k-means clustering on the input data.
+
+    Args:
+        gdf: A GeoDataFrame containing the input data.
+        number_of_clusters: The number of clusters to form.
+
+    Returns:
+        GeoDataFrame containing assigned cluster labels.
+
+    Raises:
+        EmptyDataFrameException: The input GeoDataFrame is empty.
+    """
+
+    if data.empty:
+        raise EmptyDataFrameException("The input GeoDataFrame is empty.")
+
+    k_means_gdf = _k_means_clustering(data, number_of_clusters, random_state)
+    return k_means_gdf
---
+++ tests/exploratory_analyses/k_means_cluster_test.py
@@ -0,0 +1,30 @@
+import geopandas as gdp
+import pandas as pd
+import pytest
+
+from eis_toolkit.exceptions import EmptyDataFrameException
+from eis_toolkit.exploratory_analyses.k_means_cluster import k_means_clustering
+
+df = pd.DataFrame(
+    {
+        "Location": ["A", "B", "C", "D", "E", "F"],
+        "Latitude": [1, 2, 3, 4, 5, 6],
+        "Longitude": [-1, -2, -3, -4, -5, -6],
+    }
+)
+gdf = gdp.GeoDataFrame(df, geometry=gdp.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326")
+
+
+def test_k_means_clustering_output():
+    """Test that k-means function assings data points into correct clusters."""
+    kmeans_gdf = k_means_clustering(data=gdf, number_of_clusters=2, random_state=0)
+    kmeans_labels = kmeans_gdf["cluster"]
+    expected_labels = [1, 1, 1, 0, 0, 0]
+    assert all([k == e for k, e in zip(kmeans_labels, expected_labels)])
+
+
+def test_empty_geodataframe():
+    """Test that empty geodataframe raises the correct exception."""
+    empty_gdf = gdp.GeoDataFrame()
+    with pytest.raises(EmptyDataFrameException):
+        k_means_clustering(data=empty_gdf, number_of_clusters=2)
