26af78f49dcb161ff1b53af757e7abe3e62b3c34
msorvoja <mika@gispo.fi>
2023-06-09 08:25:02 +0300

    Add elbow method for computing optimal number of clusters

--- tests/exploratory_analyses/k_means_cluster_test.py
+++ tests/exploratory_analyses/k_means_cluster_test.py
@@ -1,4 +1,5 @@
 import geopandas as gdp
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -7,9 +8,9 @@ from eis_toolkit.exploratory_analyses.k_means_cluster import k_means_clustering
 
 df = pd.DataFrame(
     {
-        "Location": ["A", "B", "C", "D", "E", "F"],
-        "Latitude": [1, 2, 3, 4, 5, 6],
-        "Longitude": [-1, -2, -3, -4, -5, -6],
+        "Location": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        "Latitude": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "Longitude": [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10],
     }
 )
 gdf = gdp.GeoDataFrame(df, geometry=gdp.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326")
@@ -19,7 +20,7 @@ def test_k_means_clustering_output():
     """Test that k-means function assings data points into correct clusters."""
     kmeans_gdf = k_means_clustering(data=gdf, number_of_clusters=2, random_state=0)
     kmeans_labels = kmeans_gdf["cluster"]
-    expected_labels = [1, 1, 1, 0, 0, 0]
+    expected_labels = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
     assert list(kmeans_labels) == expected_labels
 
 
@@ -27,6 +28,13 @@ def test_number_of_clusters():
     """Test that number of clusters given as a parameter is non-negative and greater than zero."""
     with pytest.raises(InvalidParameterValueException):
         k_means_clustering(data=gdf, number_of_clusters=0)
+
+
+def test_optimal_number_of_clusters():
+    """Test computing finding optimal number of clusters works."""
+    kmeans_gdf = k_means_clustering(data=gdf, random_state=0)
+    kmeans_clusters = len(np.unique(kmeans_gdf["cluster"]))
+    assert kmeans_clusters == 3
 
 
 def test_empty_geodataframe():
