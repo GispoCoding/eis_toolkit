bf5c1bc599255fac29085b0638b8fa04e8436d1a
Niko Aarnio <niko.aarnio@protonmail.com>
2023-06-22 09:41:35 +0300

    Pallel coordinates v2, renamed old plot function file, seaborn testing

--- eis_toolkit/exploratory_analyses/parallel_coordinates.py
+++ eis_toolkit/exploratory_analyses/parallel_coordinates.py
@@ -1,9 +1,14 @@
+from typing import Optional
+
+import matplotlib.colors as mcolors
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from beartype.typing import Sequence, Tuple
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
 from matplotlib.path import Path
 from sklearn.preprocessing import LabelEncoder
 
@@ -12,25 +17,68 @@ class ParallelCoordinatesPlot:
     """Class for parallel coordinates plot using matplotlib."""
 
     def __init__(
-        self, data: np.ndarray, categories: Sequence[int], variable_names: Sequence[str], draw_curves: bool = True
+        self,
+        data: np.ndarray,
+        data_labels: np.ndarray,
+        color_data: np.ndarray,
+        color_data_label: str,
+        palette_name: Optional[str],
+        draw_curves: bool = True,
     ):
         """Init method."""
         self.data = data
-        self.categories = categories
-        self.variable_names = variable_names
+        self.data_labels = data_labels
+        self.color_data = color_data
+        self.color_data_label = color_data_label
         self.draw_curves = draw_curves
+
+        # TODO: Add NaN check or check numerics?
+        if len(set([type(elem) for elem in color_data])) != 1:
+            raise Exception("Data used for coloring has multiple data types")
+
+        # If category_column is not numeric
+        # TODO: Add checking that labels can be transformed
+        if not np.issubdtype(type(color_data[0]), np.number):
+            self.color_data_numeric = False
+            self.label_encoder = LabelEncoder()
+            self.color_data = list(self.label_encoder.fit_transform(color_data))
+        else:
+            self.label_encoder = None
+            self.color_data_numeric = True
+
+        # If palette name is not provided, go with default
+        self.palette_name = self.get_default_palette() if not palette_name else palette_name
+
         self.num_features = data.shape[0]
+
         self.fig, self.main_axis = plt.subplots()
 
-    @classmethod
-    def from_dataframe(cls, df: pd.DataFrame, category_column: str, draw_curves=True):
-        """Create ParallelCoordinatesPlot from a dataframe."""
-        categories = df[category_column]
-        label_encoder = LabelEncoder()
-        encoded_categories = label_encoder.fit_transform(categories)
+    def get_default_palette(self):
+        if self.color_data_numeric:
+            return "Spectral"
+        else:
+            return "bright"
 
-        data = df.drop(columns=[category_column])
-        return cls(data.to_numpy(), encoded_categories, data.columns.values, draw_curves)
+    @classmethod
+    def from_dataframe(
+        cls, df: pd.DataFrame, color_column_name: str, palette_name: Optional[str] = None, draw_curves=True
+    ):
+        """Create ParallelCoordinatesPlot from a dataframe."""
+
+        color_data = df[color_column_name].to_numpy()
+
+        columns_to_drop = [color_column_name]
+        for column in df.columns.values:
+            if not np.issubdtype(df[column].dtype, np.number):
+                columns_to_drop.append(column)
+                print("Dropped column: ", column)
+
+        filtered_df = df.loc[:, ~df.columns.isin(columns_to_drop)]
+        data_labels = filtered_df.columns.values
+
+        data = filtered_df.to_numpy()
+
+        return cls(data, data_labels, color_data, color_column_name, palette_name, draw_curves)
 
     def plot(self):
         """Plot the graph."""
@@ -39,6 +87,13 @@ class ParallelCoordinatesPlot:
         self._set_style(y_min, y_max)
 
         self._draw_lines(normalized_data)
+
+        if self.color_data_numeric:
+            colorbar = plt.colorbar(self.colorbar_mappable)
+            colorbar.set_label(self.color_data_label, fontsize=14)
+
+        else:  # TODO: Couple color data numeric and discerete colors
+            plt.legend(handles=self.color_boxes)
 
         plt.tight_layout()
         plt.show()
@@ -70,83 +125,57 @@ class ParallelCoordinatesPlot:
 
         self.main_axis.set_xlim(0, self.data.shape[1] - 1)
         self.main_axis.set_xticks(range(self.data.shape[1]))
-        self.main_axis.set_xticklabels(self.variable_names, fontsize=14)
+        self.main_axis.set_xticklabels(self.data_labels, fontsize=14)
         self.main_axis.tick_params(axis="x", which="major", pad=7)
         self.main_axis.spines["right"].set_visible(False)
         self.main_axis.xaxis.tick_top()
         self.main_axis.set_title("Parallel Coordinates Plot", fontsize=18)
 
-    def _draw_lines(self, normalized_data: np.ndarray) -> None:
-        # colors = plt.cm.viridis(np.linspace(0, 1, len(np.unique(self.categories))))
-        # colors = plt.cm.tab10.colors
-        colors = sns.color_palette("bright", self.num_features)
+    # def _create_colormap(self, colors, name='custom_colormap', num_categories=None):
+    #     """Create a colormap from seaborn color palette."""
+    #     if not self.numerical_categories and num_categories <= len(colors):
+    #         colors = colors[:num_categories]  # Take the first N colors
+    #     cmap = mcolors.LinearSegmentedColormap.from_list(name, colors)
+    #     return cmap
+
+    def _draw_lines(self, data: np.ndarray) -> None:
+        self.norm = plt.Normalize(min(self.color_data), max(self.color_data))
+        cmap = sns.color_palette(self.palette_name, as_cmap=True)
+
+        # If seaborn did not produce a cmap
+        if type(cmap) == list:
+            num_categories = len(set(self.color_data))
+            # i can be larger than len(cmap) ?
+            self.color_boxes = [
+                patches.Patch(color=cmap[i], label=self.label_encoder.inverse_transform([i])[0])
+                for i in range(num_categories)
+            ]
+            if not self.color_data_numeric and num_categories <= len(cmap):
+                colors = cmap[:num_categories]  # Take the first N colors
+            cmap = mcolors.LinearSegmentedColormap.from_list("custom_colormap", colors)
+            # cmap = self._create_colormap(cmap, num_categories=num_categories)
+        else:
+            self.colorbar_mappable = ScalarMappable(cmap=cmap, norm=self.norm)
+            self.colorbar_mappable.set_array([])
+        self.cmap = cmap
 
         for i in range(self.num_features):
+            color = cmap(self.norm(self.color_data[i]))
             if self.draw_curves:
-                x = np.linspace(0, len(normalized_data) - 1, len(normalized_data) * 3 - 2, endpoint=True)
-                y = np.repeat(normalized_data[i, :], 3)[1:-1]
+                x = np.linspace(0, len(data) - 1, len(data) * 3 - 2, endpoint=True)
+                y = np.repeat(data[i, :], 3)[1:-1]
                 control_points = list(zip(x, y))
                 codes = [Path.MOVETO] + [Path.CURVE4 for _ in range(len(control_points) - 1)]
                 path = Path(control_points, codes)
-                curve_patch = patches.PathPatch(
-                    path, facecolor="none", lw=1, edgecolor=colors[self.categories[i]], alpha=0.5
-                )
+                curve_patch = patches.PathPatch(path, facecolor="none", lw=1, edgecolor=color, alpha=0.5)
                 self.main_axis.add_patch(curve_patch)
             else:
-                self.main_axis.plot(range(self.data.shape[1]), normalized_data[i, :], c=colors[self.categories[i]])
-
-    # def draw_lines(self, host, data, num_instances, categories, axis_labels, use_curves=True):
-    #     """Draws lines for each instance on the plot."""
-    #     colors = plt.cm.tab10.colors
-    #     for i in range(num_instances):
-    #         if use_curves:
-    #             # create bezier curves
-    #             x = np.linspace(0, len(data) - 1, len(data) * 3 - 2, endpoint=True)
-    #             y = np.repeat(data[i, :], 3)[1:-1]
-    #             control_points = list(zip(x, y))
-    #             codes = [Path.MOVETO] + [Path.CURVE4 for _ in range(len(control_points) - 1)]
-    #             path = Path(control_points, codes)
-    #             curve_patch = patches.PathPatch(
-    #                 path, facecolor='none', lw=1, edgecolor=colors[categories[i] - 1], alpha=0.5
-    #             )
-    #             host.add_patch(curve_patch)
-    #         else:
-    #             host.plot(range(data.shape[1]), data[i,:], c=colors[(categories[i]) % len(colors) ])
-
-
-# def plot_parallel_coordinates_from_df(df: pd.DataFrame, category_column: str, curved = True):
-#     categories = df[category_column].to_numpy()
-#     label_encoder = LabelEncoder()
-#     numerical_categories = list(label_encoder.fit_transform(categories))
-
-#     data = df.loc[:, df.columns != category_column]
-#     data_labels = data.columns.values
-#     data_arr = data.to_numpy()
-#     number_of_features = data.shape[0]
-#     plot_parallel_coordinates(data_arr, numerical_categories, data_labels, number_of_features, draw_curves = curved)
-
-
-# def plot_parallel_coordinates_dummy():
-#     ynames = ['P1', 'P2', 'P3', 'P4', 'P5']
-#     N1, N2, N3 = 10, 5, 8
-#     N = N1 + N2 + N3
-#     category = np.concatenate([np.full(N1, 1), np.full(N2, 2), np.full(N3, 3)])
-#     y1 = np.random.uniform(0, 10, N) + 7 * category
-#     y2 = np.sin(np.random.uniform(0, np.pi, N)) ** category
-#     y3 = np.random.binomial(300, 1 - category / 10, N)
-#     y4 = np.random.binomial(200, (category / 6) ** 1/3, N)
-#     y5 = np.random.uniform(0, 800, N)
-#     # organize the data
-#     ys = np.dstack([y1, y2, y3, y4, y5])[0]
-#     plot_parallel_coordinates(ys, category, ynames, N)
+                self.main_axis.plot(range(self.data.shape[1]), data[i, :], c=color)
 
 
 # PLOT IRIS DATA FROM DF
 iris = sns.load_dataset("iris")
-category_column = "species"
+color_column = "species"
 
-pl_plot = ParallelCoordinatesPlot.from_dataframe(iris, category_column)
+pl_plot = ParallelCoordinatesPlot.from_dataframe(iris, color_column)
 pl_plot.plot()
-
-# PLOT DUMMY DATA
-# plot_parallel_coordinates_dummy()
