7474017378e9705a2061bc16d368174ad3e9bf99
Michael Steffen <michael.steffen@beak.de>
2023-07-25 10:59:32 +0200

    Added unit conversions

---
+++ eis_toolkit/utilities/conversions.py
@@ -0,0 +1,47 @@
+import numpy as np
+from beartype import beartype
+
+
+@beartype
+def convert_rad_to_degree(data: np.ndarray) -> np.ndarray:
+    """
+    Convert radians to degree.
+
+    Args:
+      data: Input numpy array.
+
+    Returns:
+      The converted array in degree values.
+    """
+
+    return np.where(data >= 0, data * (180.0 / np.pi), data)
+
+
+@beartype
+def convert_rad_to_rise(data: np.ndarray) -> np.ndarray:
+    """
+    Convert radians to percent rise.
+
+    Args:
+      data: Input numpy array.
+
+    Returns:
+      The converted array in percent rise values.
+    """
+
+    return np.where(data >= 0, np.tan(data) * 100.0, data)
+
+
+@beartype
+def convert_degree_to_rad(data: np.ndarray) -> np.ndarray:
+    """
+    Convert degree to radians.
+
+    Args:
+      data: Input numpy array.
+
+    Returns:
+      The converted array in radian values.
+    """
+
+    return np.where(data >= 0, (data / 180.0) * np.pi, data)
