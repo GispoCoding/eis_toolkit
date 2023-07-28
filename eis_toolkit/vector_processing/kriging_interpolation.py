ce7c1f74715da5f3a084544d28e1f314b85b4ce7
msorvoja <mika@gispo.fi>
2023-07-28 09:59:57 +0300

    Add universal kriging method and tests

--- eis_toolkit/vector_processing/kriging_interpolation.py
+++ eis_toolkit/vector_processing/kriging_interpolation.py
@@ -6,6 +6,7 @@ import numpy.ma as ma
 from beartype import beartype
 from beartype.typing import Tuple
 from pykrige.ok import OrdinaryKriging
+from pykrige.uk import UniversalKriging
 
 from eis_toolkit.exceptions import (
     EmptyDataFrameException,
@@ -19,6 +20,8 @@ def _kriging(
     resolution: Tuple[Number, Number],
     extent: Tuple[Number, Number, Number, Number],
     variogram_model: str,
+    method: str,
+    drift_terms: list,
 ) -> Tuple[np.ndarray, dict]:
 
     coordinates = np.array(list(data.geometry.apply(lambda geom: [geom.x, geom.y, geom.z])))
@@ -29,8 +32,13 @@ def _kriging(
     grid_x = np.linspace(extent[0], extent[1], resolution[0])
     grid_y = np.linspace(extent[2], extent[3], resolution[1])
 
-    ordinary_kriging = OrdinaryKriging(x, y, z, variogram_model)
-    z_interpolated, _ = ordinary_kriging.execute("grid", grid_x, grid_y)
+    if method == "universal":
+        universal_kriging = UniversalKriging(x, y, z, variogram_model=variogram_model, drift_terms=drift_terms)
+        z_interpolated, _ = universal_kriging.execute("grid", grid_x, grid_y)
+
+    if method == "ordinary":
+        ordinary_kriging = OrdinaryKriging(x, y, z, variogram_model=variogram_model)
+        z_interpolated, _ = ordinary_kriging.execute("grid", grid_x, grid_y)
 
     z_interpolated = ma.getdata(z_interpolated)
 
@@ -45,6 +53,8 @@ def kriging(
     resolution: Tuple[Number, Number],
     extent: Tuple[Number, Number, Number, Number],
     variogram_model: str = "linear",
+    method: str = "ordinary",
+    drift_terms: list = ["regional_linear"],
 ) -> Tuple[np.ndarray, dict]:
     """
     Perform Kriging interpolation on the input data.
@@ -54,13 +64,16 @@ def kriging(
         resolution: Size of the output grid.
         extent: Limits of the output grid.
         variogram_model: Variogram model to be used. Defaults to 'linear'.
+        method: Kriging method. Defaults to 'ordinary'.
+        drift_terms: Drift terms used in universal kriging.
 
     Returns:
         Grid containing the interpolated values and metadata.
 
     Raises:
         EmptyDataFrameException: The input GeoDataFrame is empty.
-        InvalidParameterValueException: The resolution is not greater than zero or variogram model is invalid.
+        InvalidParameterValueException: The resolution is not greater than zero, variogram model is invalid,
+                                        kriging method is invalid or any input drift term is invalid.
         NotApplicableGeometryTypeException: GeoDataFrame's geometry is missing z coordinates.
     """
 
@@ -78,6 +91,16 @@ def kriging(
             "Variogram model must be 'linear', 'power', 'gaussian', 'spherical', 'exponential' or 'hole-effect'."
         )
 
-    data_interpolated, out_meta = _kriging(data, resolution, extent, variogram_model)
+    if method not in ("ordinary", "universal"):
+        raise InvalidParameterValueException("Kriging method must be either 'ordinary' or 'universal'.")
+
+    if any(
+        term not in ("regional_linear", "point_log", "external_Z", "specified", "functional") for term in drift_terms
+    ):
+        raise InvalidParameterValueException(
+            "Accepted drift terms are 'regional_linear', 'point_log', 'external_Z', 'specified' and 'functional'."
+        )
+
+    data_interpolated, out_meta = _kriging(data, resolution, extent, variogram_model, method, drift_terms)
 
     return data_interpolated, out_meta
--- tests/vector_processing/test_kriging_interpolation.py
+++ tests/vector_processing/test_kriging_interpolation.py
@@ -10,22 +10,34 @@ from eis_toolkit.exceptions import (
 )
 from eis_toolkit.vector_processing.kriging_interpolation import kriging
 
-np.random.seed(6)
+np.random.seed(0)
 x = np.random.uniform(0, 5, size=(10, 1))
 y = np.random.uniform(0, 5, size=(10, 1))
 z = np.random.uniform(0, 2, size=(10, 1))
 data = np.hstack((x, y, z))
-
 df = pd.DataFrame(data, columns=["x", "y", "z"])
 gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(df["x"], df["y"], df["z"]))
+resolution = (10, 10)
+extent = (0, 5, 0, 5)
 
 
-def test_kriging_output():
-    """Test that kriging output has correct shape and values."""
-    z_interpolated, out_meta = kriging(data=gdf, resolution=(10, 10), extent=(0, 5, 0, 5))
-    expected_shape = (10, 10)
-    expected_value_first_pixel = 1.47416754
-    expected_value_last_pixel = 0.73852108
+def test_ordinary_kriging_output():
+    """Test that ordinary kriging output has correct shape and values."""
+    z_interpolated, _ = kriging(data=gdf, resolution=resolution, extent=extent)
+    expected_shape = resolution
+    expected_value_first_pixel = 0.4831651
+    expected_value_last_pixel = 1.51876801
+    assert z_interpolated.shape == expected_shape
+    assert round(z_interpolated[0][0], 8) == expected_value_first_pixel
+    assert round(z_interpolated[9][9], 8) == expected_value_last_pixel
+
+
+def test_universal_kriging_output():
+    """Test that universal kriging output has correct shape and values."""
+    z_interpolated, _ = kriging(data=gdf, resolution=resolution, extent=extent, method="universal")
+    expected_shape = resolution
+    expected_value_first_pixel = -0.21513566
+    expected_value_last_pixel = 1.86445049
     assert z_interpolated.shape == expected_shape
     assert round(z_interpolated[0][0], 8) == expected_value_first_pixel
     assert round(z_interpolated[9][9], 8) == expected_value_last_pixel
@@ -35,23 +47,41 @@ def test_empty_geodataframe():
     """Test that empty geodataframe raises the correct exception."""
     empty_gdf = gpd.GeoDataFrame()
     with pytest.raises(EmptyDataFrameException):
-        kriging(data=empty_gdf, resolution=(10, 10), extent=(0, 5, 0, 5))
+        kriging(data=empty_gdf, resolution=resolution, extent=extent)
 
 
 def test_invalid_resolution():
     """Test that invalid resolution raises the correct exception."""
     with pytest.raises(InvalidParameterValueException):
-        kriging(data=gdf, resolution=(0, 0), extent=(0, 5, 0, 5))
+        kriging(data=gdf, resolution=(0, 0), extent=extent)
 
 
 def test_invalid_geometry():
     """Test that invalid geometries raise the correct exception."""
     gdf_invalid_geometry = gpd.GeoDataFrame(geometry=gpd.points_from_xy(df["x"], df["y"]))
     with pytest.raises(NotApplicableGeometryTypeException):
-        kriging(data=gdf_invalid_geometry, resolution=(10, 10), extent=(0, 5, 0, 5))
+        kriging(data=gdf_invalid_geometry, resolution=resolution, extent=extent)
 
 
 def test_invalid_variogram_model():
     """Test that invalid variogram model raises the correct exception."""
     with pytest.raises(InvalidParameterValueException):
-        kriging(gdf, resolution=(10, 10), extent=(0, 5, 0, 5), variogram_model="invalid_model")
+        kriging(data=gdf, resolution=resolution, extent=extent, variogram_model="invalid_model")
+
+
+def test_invalid_method():
+    """Test that invalid kriging method raises the correct exception."""
+    with pytest.raises(InvalidParameterValueException):
+        kriging(data=gdf, resolution=resolution, extent=extent, method="invalid_method")
+
+
+def test_invalid_drift_term():
+    """Test that invalid drift term raises the correct exception."""
+    with pytest.raises(InvalidParameterValueException):
+        kriging(
+            data=gdf,
+            resolution=resolution,
+            extent=extent,
+            method="universal",
+            drift_terms=["regional_linear", "invalid_drift_term"],
+        )
