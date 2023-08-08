ea8aabd2964344d40b00e4483b4199de4923e2ce
chudasama-bijal <bijal.chudasama@gtk.fi>
2023-08-08 12:57:09 +0300

    added Class Weightsof EvidenceType

--- eis_toolkit/prediction/weights_of_evidence/weights_type.py
+++ eis_toolkit/prediction/weights_of_evidence/weights_type.py
@@ -1,5 +1,23 @@
 from typing import Tuple
 import pandas as pd
+from enum import Enum
+
+class WeightsOfEvidenceType(Enum):
+    Unique = 0
+    CumulativeAscending = 1
+    CumulativeDescending = 2
+
+    def __str__(self):
+        return f'{self.name.lower()}({self.value})'
+    
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.value == other
+    
+        if isinstance(other, WeightsOfEvidenceType):
+            return self is other
+    
+        return False
 
 def _weights_type(
     df: pd.DataFrame, nan_val: float, w_type: int = 0
@@ -12,25 +30,27 @@ def _weights_type(
     df_rcl_nan = df_rcl.get_group('NaN')
 
     pd.set_option('mode.chained_assignment', None)        
-    if (w_type == 0):
+    if w_type == WeightsOfEvidenceType.Unique:
         #returns the df with the data classes  for categorical weights calculations
         return df_rcl_dt, df_rcl_nan
         #for sorting of classes for numerical weights calculations
-    elif (w_type != 0):
+    else: #w_type != 0:
         df_rcl_dt.rename(columns = {"Point_Count":"Act_Point_Count"}, inplace = True)
-        if (w_type == 1):
+        if w_type == WeightsOfEvidenceType.CumulativeAscending:
             df_srtd = df_rcl_dt.sort_values(by = "Class", ascending=True)
             #return df_srtd
-        elif (w_type == 2):
+        elif w_type == WeightsOfEvidenceType.CumulativeDescending:
             df_srtd = df_rcl_dt.sort_values(by = "Class", ascending=False)    
     
         pd.set_option('mode.chained_assignment', None)   
-        return (df_srtd
+        df_data_srtd = (df_srtd
                 .assign(Point_Count = lambda df_srtd: df_srtd.Act_Point_Count.cumsum().replace(0,0.0001))
                 .assign(cmltv_cnt = lambda df_srtd: df_srtd.Count.cumsum())
-                .assign(No_Dep_Cnt = lambda df_srtd: df_srtd.No_Dep_Cnt.cumsum().replace(0, 0.0001)),
-                df_rcl_nan
-                )
+                .assign(No_Dep_Cnt = lambda df_srtd: df_srtd.No_Dep_Cnt.cumsum().replace(0, 0.0001))
+                 )
+        return df_data_srtd, df_rcl_nan
+
+        
    
 def weights_type(
         df: pd.DataFrame,  nan_val: float, w_type: int = 0
@@ -44,8 +64,7 @@ def weights_type(
     Returns:
         df_data (pandas.DataFrame): The dataframe with data values and sorted is weights calculations type is numerical (i.e., w_type = 1 or 2)
         df_nan (pandas.DataFrame): The dataframe with information on NoData
-    Raises:
-        
+            
     """
     w_type_acc = [0,1,2]
     if w_type not in w_type_acc:
