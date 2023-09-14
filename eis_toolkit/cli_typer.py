213fd40a521f065a6f30868166b6283fd93a5c0c
Niko Aarnio <niko.aarnio@protonmail.com>
2023-09-14 12:55:06 +0300

    Added CLI implementations for multiple tools, updated some implementations

--- eis_toolkit/cli_typer.py
+++ eis_toolkit/cli_typer.py
@@ -1,18 +1,14 @@
+import json
 from enum import Enum
 from pathlib import Path
 from typing import List, Tuple
 
 import geopandas as gpd
+import pandas as pd
 import rasterio
 import typer
 from rasterio import warp
 from typing_extensions import Annotated
-
-from eis_toolkit.raster_processing.clipping import clip_raster
-from eis_toolkit.raster_processing.reprojecting import reproject_raster
-from eis_toolkit.raster_processing.snapping import snap_with_raster
-from eis_toolkit.raster_processing.windowing import extract_window
-from eis_toolkit.vector_processing.reproject_vector import reproject_vector
 
 app = typer.Typer()
 
@@ -28,6 +24,13 @@ class VariogramModel(str, Enum):
     hole_effect = "hole-effect"
 
 
+class CoordinatesType(str, Enum):
+    """Coordinates type for kriging interpolation."""
+
+    euclidean = "euclidean"
+    geographic = "geographic"
+
+
 class KrigingMethod(str, Enum):
     """Kriging methods."""
 
@@ -40,6 +43,13 @@ class MergeStrategy(str, Enum):
 
     replace = "replace"
     add = "add"
+
+
+class VectorDensityStatistic(str, Enum):
+    """Vector density statistic."""
+
+    density = "density"
+    count = "count"
 
 
 class ResamplingMethods(str, Enum):
@@ -97,6 +107,13 @@ OUTPUT_FILE_OPTION = typer.Option(
 )
 
 
+# --- RASTER PROCESSING ---
+
+
+# CHECK RASTER GRIDS
+# TODO
+
+
 # CLIP RASTER
 @app.command()
 def clip_raster_cli(
@@ -105,6 +122,7 @@ def clip_raster_cli(
     output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
 ):
     """Clip the input raster with geometries in a geodataframe."""
+    from eis_toolkit.raster_processing.clipping import clip_raster
 
     typer.echo("Progress: 10%")
 
@@ -126,6 +144,90 @@ def clip_raster_cli(
     typer.echo(f"Clipping completed, output raster written to {output_raster}.")
 
 
+# CREATE CONSTANT RASTER
+@app.command()
+def create_constant_raster_cli(
+    output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
+    constant_value: float = typer.Option(),
+    template_raster: Annotated[Path, INPUT_FILE_OPTION] = None,
+    coord_west: float = None,
+    coord_north: float = None,
+    coord_east: float = None,
+    coord_south: float = None,
+    target_epsg: int = None,
+    target_pixel_size: int = None,
+    raster_width: int = None,
+    raster_height: int = None,
+    nodata_value: float = None,
+):
+    """
+    Create a constant raster with the given value.
+
+    There are 3 methods for raster creation:
+    - Set extent and coordinate system based on a template raster.
+    - Set extent from origin, based on the western and northern coordinates and the pixel size.
+    - Set extent from bounds, based on western, northern, eastern and southern points.
+    """
+    from eis_toolkit.raster_processing.create_constant_raster import create_constant_raster
+
+    if template_raster is not None:
+        with rasterio.open(template_raster) as raster:
+            out_image, out_meta = create_constant_raster(
+                constant_value,
+                raster,
+                coord_west,
+                coord_north,
+                coord_east,
+                coord_south,
+                target_epsg,
+                target_pixel_size,
+                raster_width,
+                raster_height,
+                nodata_value,
+            )
+    else:
+        out_image, out_meta = create_constant_raster(
+            constant_value,
+            template_raster,
+            coord_west,
+            coord_north,
+            coord_east,
+            coord_south,
+            target_epsg,
+            target_pixel_size,
+            raster_width,
+            raster_height,
+            nodata_value,
+        )
+
+    with rasterio.open(output_raster, "w", **out_meta) as dest:
+        dest.write(out_image)
+
+    typer.echo("Creating constant raster completed")
+    typer.echo(f"Writing raster to {output_raster}.")
+
+
+# EXTRACT VALUES FROM RASTER
+@app.command()
+def extract_values_from_raster_cli(
+    input_raster: Annotated[Path, INPUT_FILE_OPTION],
+    geometries: Annotated[Path, INPUT_FILE_OPTION],
+    output_vector: Annotated[Path, OUTPUT_FILE_OPTION],
+):
+    """Extract raster values using point data to a DataFrame."""
+    from eis_toolkit.raster_processing.extract_values_from_raster import extract_values_from_raster
+
+    geodataframe = gpd.read_file(geometries)
+
+    with rasterio.open(input_raster) as raster:
+        df = extract_values_from_raster(raster=[raster], geodataframe=geodataframe)
+
+    df.to_csv(output_vector)
+
+    typer.echo("Extracting values from raster completed")
+    typer.echo(f"Writing vector to {output_vector}.")
+
+
 # REPROJECT RASTER
 @app.command()
 def reproject_raster_cli(
@@ -135,6 +237,8 @@ def reproject_raster_cli(
     resampling_method: ResamplingMethods = typer.Option(help="resample help", default=ResamplingMethods.nearest),
 ):
     """Reproject the input raster to given CRS."""
+    from eis_toolkit.raster_processing.reprojecting import reproject_raster
+
     method = RESAMPLING_MAPPING[resampling_method]
     with rasterio.open(input_raster) as raster:
         out_image, out_meta = reproject_raster(raster=raster, target_crs=target_crs, resampling_method=method)
@@ -154,6 +258,8 @@ def snap_raster_cli(
     output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
 ):
     """Snaps/aligns input raster to the given snap raster."""
+    from eis_toolkit.raster_processing.snapping import snap_with_raster
+
     with rasterio.open(input_raster) as src, rasterio.open(snap_raster) as snap_src:
         out_image, out_meta = snap_with_raster(src, snap_src)
 
@@ -161,26 +267,6 @@ def snap_raster_cli(
         dst.write(out_image)
 
     typer.echo("Snapping completed")
-    typer.echo(f"Writing raster to {output_raster}")
-
-
-# EXTRACT WINDOW
-@app.command()
-def extract_window_cli(
-    input_raster: Annotated[Path, INPUT_FILE_OPTION],
-    output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
-    center_coords: Tuple[float, float] = typer.Option(),
-    height: int = typer.Option(),
-    width: int = typer.Option(),
-):
-    """Extract window from raster."""
-    with rasterio.open(input_raster) as raster:
-        out_image, out_meta = extract_window(raster, center_coords, height, width)
-
-    with rasterio.open(output_raster, "w", **out_meta) as dst:
-        dst.write(out_image)
-
-    typer.echo("Windowing completed")
     typer.echo(f"Writing raster to {output_raster}")
 
 
@@ -194,7 +280,115 @@ def unify_rasters_cli(
     same_extent: bool = False,
 ):
     """Extract window from raster. NOT IMPLEMENTED YET."""
+    # TODO
     pass
+
+
+# EXTRACT WINDOW
+@app.command()
+def extract_window_cli(
+    input_raster: Annotated[Path, INPUT_FILE_OPTION],
+    output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
+    center_coords: Tuple[float, float] = typer.Option(),
+    height: int = typer.Option(),
+    width: int = typer.Option(),
+):
+    """Extract window from raster."""
+    from eis_toolkit.raster_processing.windowing import extract_window
+
+    with rasterio.open(input_raster) as raster:
+        out_image, out_meta = extract_window(raster, center_coords, height, width)
+
+    with rasterio.open(output_raster, "w", **out_meta) as dst:
+        dst.write(out_image)
+
+    typer.echo("Windowing completed")
+    typer.echo(f"Writing raster to {output_raster}")
+
+
+# --- VECTOR PROCESSING ---
+
+
+# IDW INTERPOLATION
+@app.command()
+def idw_interpolation_cli(
+    input_vector: Annotated[Path, INPUT_FILE_OPTION],
+    output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
+    target_column: str = typer.Option(),
+    pixel_size_x: float = typer.Option(),
+    pixel_size_y: float = typer.Option(),
+    power: float = 2.0,
+    extent: Tuple[float, float, float, float] = None,
+):
+    """Apply kriging interpolation to input vector file."""
+    from eis_toolkit.vector_processing.idw_interpolation import idw
+
+    geodataframe = gpd.read_file(input_vector)
+
+    out_image, out_meta = idw(
+        geodataframe=geodataframe,
+        target_column=target_column,
+        resolution=(pixel_size_x, pixel_size_y),
+        extent=extent,
+        power=power,
+    )
+
+    out_meta.update(
+        {
+            "count": 1,
+            "driver": "GTiff",
+            "dtype": "float32",
+        }
+    )
+
+    with rasterio.open(out_image, "w", **out_meta) as dst:
+        dst.write(out_image)
+
+    typer.echo("IDW interpolation completed")
+    typer.echo(f"Writing raster to {output_raster}")
+
+
+# KRIGING INTERPOLATION
+@app.command()
+def kriging_interpolation_cli(
+    input_vector: Annotated[Path, INPUT_FILE_OPTION],
+    output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
+    target_column: str = typer.Option(),
+    pixel_size_x: float = typer.Option(),
+    pixel_size_y: float = typer.Option(),
+    extent: Tuple[float, float, float, float] = None,
+    variogram_model: VariogramModel = VariogramModel.linear,
+    coordinates_type: CoordinatesType = CoordinatesType.geographic,
+    method: KrigingMethod = KrigingMethod.ordinary,
+):
+    """Apply kriging interpolation to input vector file."""
+    from eis_toolkit.vector_processing.kriging_interpolation import kriging
+
+    geodataframe = gpd.read_file(input_vector)
+
+    out_image, out_meta = kriging(
+        data=geodataframe,
+        target_column=target_column,
+        resolution=(pixel_size_x, pixel_size_y),
+        extent=extent,
+        variogram_model=variogram_model,
+        coordinates_type=coordinates_type,
+        method=method,
+    )
+
+    out_meta.update(
+        {
+            "count": 1,
+            "driver": "GTiff",
+            "dtype": "float32",
+        }
+    )
+
+    with rasterio.open(out_image, "w", **out_meta) as dst:
+        dst.write(out_image)
+
+    typer.echo("Kriging interpolation completed")
+    typer.echo(f"Writing raster to {output_raster}")
 
 
 # RASTERIZE
@@ -252,6 +446,8 @@ def reproject_vector_cli(
     target_crs: int = typer.Option(help="crs help"),
 ):
     """Reproject the input vector to given CRS."""
+    from eis_toolkit.vector_processing.reproject_vector import reproject_vector
+
     geodataframe = gpd.read_file(input_vector)
 
     reprojected_geodataframe = reproject_vector(geodataframe=geodataframe, target_crs=target_crs)
@@ -262,47 +458,48 @@ def reproject_vector_cli(
     typer.echo(f"Writing vector to {output_vector}")
 
 
-# KRIGING INTERPOLATION
+# VECTOR DENSITY
 @app.command()
-def kriging_interpolation_cli(
+def vector_density_cli(
     input_vector: Annotated[Path, INPUT_FILE_OPTION],
     output_raster: Annotated[Path, OUTPUT_FILE_OPTION],
-    target_column: str = typer.Option(),
-    pixel_size_x: float = typer.Option(),
-    pixel_size_y: float = typer.Option(),
-    extent: Tuple[float, float, float, float] = None,
-    variogram_model: VariogramModel = VariogramModel.linear,
-    method: KrigingMethod = KrigingMethod.ordinary,
-    drift_terms: Annotated[List[str], typer.Option()] = ["regional_linear"],
+    resolution: float = None,
+    base_raster_profile_raster: Annotated[Path, INPUT_FILE_OPTION] = None,
+    buffer_value: float = None,
+    statistic: VectorDensityStatistic = VectorDensityStatistic.density,
 ):
-    """Apply kriging interpolation to input vector file."""
-    from eis_toolkit.vector_processing.kriging_interpolation import kriging
+    """Reproject the input vector to given CRS."""
+    from eis_toolkit.vector_processing.vector_density import vector_density
 
     geodataframe = gpd.read_file(input_vector)
 
-    out_image, out_meta = kriging(
-        data=geodataframe,
-        target_column=target_column,
-        resolution=(pixel_size_x, pixel_size_y),
-        extent=extent,
-        variogram_model=variogram_model,
-        method=method,
-        drift_terms=drift_terms,
+    if base_raster_profile_raster is not None:
+        with rasterio.open(base_raster_profile_raster) as raster:
+            base_raster_profile = raster.profile
+
+    out_image, out_meta = vector_density(
+        geodataframe=geodataframe,
+        resolution=resolution,
+        base_raster_profile=base_raster_profile,
+        buffer_value=buffer_value,
+        statistic=statistic,
     )
 
     out_meta.update(
         {
             "count": 1,
-            "driver": "GTiff",
-            "dtype": "float32",
+            "dtype": base_raster_profile["dtype"] if base_raster_profile_raster else float,  # TODO change this
         }
     )
 
-    with rasterio.open(out_image, "w", **out_meta) as dst:
+    with rasterio.open(output_raster, "w", **out_meta) as dst:
         dst.write(out_image)
 
-    typer.echo("Kriging interpolation completed")
+    typer.echo("Vector density computation completed")
     typer.echo(f"Writing raster to {output_raster}")
+
+
+# --- SPATIAL ANALYSES ---
 
 
 # DISTANCE COMPUTATION
@@ -329,10 +526,82 @@ def distance_computation_cli(
     typer.echo(f"Writing raster to {output_raster}")
 
 
+# CBA
+# TODO
+
+
+# --- STATISTICAL ANALYSES ---
+
+
 # DESCRIPTIVE STATISTICS
-def descriptive_statistics_cli(input_file: Annotated[Path, INPUT_FILE_OPTION], column: str):
-    """Not implemented yet."""
-    pass
+@app.command()
+def descriptive_statistics_cli(input_file: Annotated[Path, INPUT_FILE_OPTION], column: str = None):
+    """Generate descriptive statistics from raster, vector or tabular data."""
+    from eis_toolkit.statistical_analyses.descriptive_statistics import (
+        descriptive_statistics_dataframe,
+        descriptive_statistics_raster,
+    )
+
+    # TODO modify input file detection
+    try:
+        raster = rasterio.open(input_file)
+        results_dict = descriptive_statistics_raster(raster)
+    except:  # noqa: E722  # TODO Which exception? Applies to the 2 below as well
+        try:
+            gdf = gpd.read_file(input_file)
+            results_dict = descriptive_statistics_dataframe(gdf, column)
+        except:  # noqa: E722
+            try:
+                df = pd.read_csv(input_file)
+                results_dict = descriptive_statistics_dataframe(df, column)
+            except:  # noqa: E722
+                raise Exception("Could not read input file as raster or dataframe")
+
+    json_str = json.dumps(results_dict)
+    typer.echo(f"Results: {json_str}")
+    typer.echo("Descriptive statistics completed")
+
+
+# --- PREDICTION ---
+
+
+# FUZZY OVERLAY
+# TODO
+
+
+# WOFE
+# TODO
+
+
+# --- TRANSFORMATIONS ---
+
+
+# BINARIZE
+# TODO
+
+
+# CLIPPING (PROBLEMATIC NAME, CHANGE!)
+# TODO
+
+
+# LINEAR
+# TODO
+
+
+# LOGARITHMIC
+# TODO
+
+
+# SIGMOID
+# TODO
+
+
+# WINSORIZE
+# TODO
+
+
+# ---VALIDATION ---
+# TODO
 
 
 # if __name__ == "__main__":
