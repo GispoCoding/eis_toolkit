a5b3223f9c9acfeeb8d2197de7e635e9233951bf
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2023-11-02 15:53:17 +0200

    Clean up reclassify raster test file

--- tests/raster_processing/reclassify_raster_test.py
+++ tests/raster_processing/reclassify_raster_test.py
@@ -4,19 +4,22 @@ import numpy as np
 import pytest
 import rasterio
 
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_defined_intervals
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_equal_intervals
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_geometrical_intervals
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_manual_breaks
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_natural_breaks
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_quantiles
-from eis_toolkit.raster_processing.reclassify_raster import raster_with_standard_deviation
+from eis_toolkit.raster_processing.reclassify_raster import (
+    raster_with_defined_intervals,
+    raster_with_equal_intervals,
+    raster_with_geometrical_intervals,
+    raster_with_manual_breaks,
+    raster_with_natural_breaks,
+    raster_with_quantiles,
+    raster_with_standard_deviation,
+)
 
 test_dir = Path(__file__).parent.parent
 raster_path = test_dir.joinpath("data/remote/small_raster.tif")
 raster_copy_path = test_dir.joinpath("data/local/small_raster - Copy.tif")
 
 band_numbers = [1]
+
 
 def test_raster_with_defined_intervals():
     """Test raster with defined intervals by comparing the output of the function to the original data."""
@@ -46,7 +49,6 @@ def raster_with_equal_intervals():
         expected_result = np.digitize(band_1, expected_intervals)
 
         assert np.array_equal(output.read(1), expected_result)
-    
 
 
 def test_raster_with_geometrical_intervals():
@@ -63,7 +65,7 @@ def test_raster_with_geometrical_intervals():
 
 def test_raster_with_manual_breaks():
     """Test raster with manual break intervals by comparing the output of the function to numpy's digitized result."""
-    with rasterio.open(raster_path, 'r+') as raster:
+    with rasterio.open(raster_path, "r+") as raster:
 
         breaks = [-2000, -1000, 500, 1000]
 
@@ -74,13 +76,13 @@ def test_raster_with_manual_breaks():
         expected_result = np.digitize(band_1, breaks)
 
         assert np.array_equal(output.read(1), expected_result)
-    
-    
+
+
 def test_raster_with_natural_breaks():
     """Test raster with natural break intervals by comparing the output of the function to the original data."""
     number_of_classes = 10
 
-    raster = rasterio.open(raster_path, 'r+')
+    raster = rasterio.open(raster_path, "r+")
     band_1 = raster.read(1)
 
     output = raster_with_natural_breaks(raster, number_of_classes, raster_copy_path, band_numbers)
@@ -92,7 +94,7 @@ def raster_with_standard_deviation():
     """Test raster with standard deviation intervals by comparing the output of the function to the original data."""
     intervals = 75
 
-    raster = rasterio.open(raster_path, 'r+')
+    raster = rasterio.open(raster_path, "r+")
     band_1 = raster.read(1)
 
     output = raster_with_standard_deviation(raster, intervals, raster_copy_path, band_numbers)
@@ -104,10 +106,9 @@ def test_raster_with_quantiles():
     """Test raster with quantile intervals by comparing the output of the function to the original data."""
     quantiles = 4
 
-    raster = rasterio.open(raster_path, 'r+')
+    raster = rasterio.open(raster_path, "r+")
     band_1 = raster.read(1)
 
     output = raster_with_quantiles(raster, quantiles, raster_copy_path, band_numbers)
 
     assert not np.array_equal(output.read(1), band_1)
-    
