676839c1855459a2f52e466be79ea2b23d0983ac
Emmu T <emmu.taivalsaari@gtk.fi>
2023-11-09 16:53:51 +0200

    Fix CLR logic.

--- eis_toolkit/transformations/coda/clr.py
+++ eis_toolkit/transformations/coda/clr.py
@@ -2,14 +2,25 @@ import numpy as np
 import pandas as pd
 from beartype import beartype
 from beartype.typing import Optional, Sequence
+from scipy.stats import gmean
 
 from eis_toolkit.exceptions import InvalidColumnException
 from eis_toolkit.utilities.checks.dataframe import check_columns_valid, check_dataframe_contains_nonzero_numbers
 
 
 @beartype
+def _centered_ratio(row: pd.Series) -> pd.Series:
+    """TODO: docstring."""
+    N = len(row) * 1.0
+    return row / np.float_power(gmean(row), 1 / N)
+
+
+@beartype
 def _CLR_transform(df: pd.DataFrame, columns: Optional[Sequence[str]] = None) -> pd.DataFrame:
     """TODO: docstring."""
+
+    # TODO: deal with potential negative values
+
     if columns is not None:
         if check_columns_valid(df, columns) is False:
             raise InvalidColumnException("Not all of the given columns were found in the input DataFrame.")
@@ -21,7 +32,7 @@ def _CLR_transform(df: pd.DataFrame, columns: Optional[Sequence[str]] = None) ->
 
     dfc = df.copy()
 
-    dfc = dfc[columns].div(df.sum(axis=1), axis=0)
+    dfc = dfc[columns].apply(_centered_ratio, axis=1)
 
     return np.log(dfc)
 
--- tests/transformations/coda/clr_test.py
+++ tests/transformations/coda/clr_test.py
@@ -7,9 +7,9 @@ from eis_toolkit.transformations.coda.clr import _CLR_transform
 
 ONES_DATAFRAME_4x4 = pd.DataFrame(np.ones((4, 4)), columns=["c1", "c2", "c3", "c4"])
 
-LN_RESULT = np.log(0.25)
+ZEROS_DATAFRAME_4x4 = pd.DataFrame(np.zeros((4, 4)), columns=["c1", "c2", "c3", "c4"])
 
-ONES_DATAFRAME_TRANSFORMED = pd.DataFrame(np.full((4, 4), LN_RESULT), columns=["c1", "c2", "c3", "c4"])
+SIMPLE_ROW_1 = pd.DataFrame(np.array([1, 1, 1, 2])[None], columns=["c1", "c2", "c3", "c4"])
 
 SAMPLE_DATAFRAME = pd.DataFrame(
     np.array(
@@ -30,10 +30,23 @@ SAMPLE_DATAFRAME = pd.DataFrame(
 )
 
 
+def test_clr_transform_subcomposition():
+    """TODO: docstring."""
+    subcomposition = ["c1", "c2", "c3"]
+    result = _CLR_transform(SIMPLE_ROW_1, subcomposition)
+    pd.testing.assert_frame_equal(pd.DataFrame(np.zeros((1, 3)), columns=subcomposition), result)
+
+
+def test_clr_transform_subcomposition_single_component():
+    """TODO: docstring."""
+    result = _CLR_transform(SIMPLE_ROW_1, ["c4"])
+    pd.testing.assert_frame_equal(pd.DataFrame(np.zeros((1, 1)), columns=["c4"]), result)
+
+
 def test_clr_transform_simple():
     """TODO: docstring."""
     result = _CLR_transform(ONES_DATAFRAME_4x4)
-    pd.testing.assert_frame_equal(result, ONES_DATAFRAME_TRANSFORMED)
+    pd.testing.assert_frame_equal(result, ZEROS_DATAFRAME_4x4)
 
 
 def test_clr_transform_subset_returns_correct_size():
@@ -45,7 +58,7 @@ def test_clr_transform_subset_returns_correct_size():
 def test_clr_transform_contains_zeros():
     """TODO: docstring."""
     with pytest.raises(InvalidColumnException):
-        df = ONES_DATAFRAME_4x4.copy()
+        df = SAMPLE_DATAFRAME.copy()
         df.iloc[0, 0] = 0
         _CLR_transform(df)
 
