289c4118b6bdefb93ad11edbc14a167d81be1563
Niko Aarnio <niko.aarnio@protonmail.com>
2023-11-10 10:02:48 +0200

    Change exception check from test

--- tests/validation/plot_rate_curve_test.py
+++ tests/validation/plot_rate_curve_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from beartype import roar
 
 from eis_toolkit.exceptions import InvalidParameterValueException
 from eis_toolkit.validation.plot_rate_curve import plot_rate_curve
@@ -7,7 +8,7 @@ from eis_toolkit.validation.plot_rate_curve import plot_rate_curve
 
 def test_plot_rate_curve_invalid_parameter_value():
     """Tests that invalid parameter value raises the correct exception."""
-    with pytest.raises(InvalidParameterValueException):
+    with pytest.raises(roar.BeartypeCallHintParamViolation):
         x_values = np.linspace(0, 1, 10)
         y_values = np.linspace(0, 1, 10)
         plot_rate_curve(x_values=x_values, y_values=y_values, plot_type="scatter_plot")
