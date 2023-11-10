9ecc820e5a4d36bbec87f59fa5b9c709633c6ba2
Emmu T <emmu.taivalsaari@gtk.fi>
2023-11-10 14:31:28 +0200

    Add a utility module called aitchison_geometry to store functions common to coda transforms.

---
+++ eis_toolkit/utilities/aitchison_geometry.py
@@ -0,0 +1,11 @@
+# TODO: functions to ensure compositions belong to the simplex sample space
+
+# TODO: closure, aka function to normalize composition to the standard simplex
+
+# TODO (below): operations in the Aitchison geometry/simplex
+
+# (POSSIBLE TODO: perturbation operation function)
+
+# (POSSIBLE TODO: powering operation function)
+
+# (POSSIBLE TODO: inner product operation)
