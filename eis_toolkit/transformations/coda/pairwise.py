2ff3ca581c1e4af3566062b79e19e49996b535b9
Emmu T <emmu.taivalsaari@gtk.fi>
2023-11-24 13:25:16 +0200

    Implement pairwise logratio transform for two columns of a dataframe.

--- eis_toolkit/transformations/coda/pairwise.py
+++ eis_toolkit/transformations/coda/pairwise.py
@@ -1,7 +1,8 @@
 import numpy as np
+import pandas as pd
 from beartype import beartype
 
-from eis_toolkit.exceptions import InvalidParameterValueException
+from eis_toolkit.exceptions import InvalidColumnException, InvalidParameterValueException
 
 
 @beartype
@@ -29,3 +30,36 @@ def single_pairwise_logratio(x1: float, x2: float) -> np.float64:
         raise InvalidParameterValueException("Input values cannot be zero.")
 
     return _single_pairwise_logratio(x1, x2)
+
+
+@beartype
+def _pairwise_logratio(df: pd.DataFrame, c1: str, c2: str) -> pd.Series:
+    dfc = df.copy()
+    dfc[[c1, c2]] = dfc[[c1, c2]].astype(float)
+
+    result = pd.Series([0.0] * df.shape[0])
+
+    for idx, row in dfc.iterrows():
+        result[idx] = single_pairwise_logratio(row[c1], row[c2])
+
+    return result
+
+
+@beartype
+def pairwise_logratio(df: pd.DataFrame, c1: str, c2: str) -> pd.Series:
+    """
+    Perform a pairwise logratio transformation on the given columns.
+
+    Args:
+        df: The dataframe containing the columns to use in the transformation.
+
+    Returns:
+        A series containing the transformed values.
+
+    Raises:
+        InvalidColumnException: One or both of the input columns are not found in the dataframe.
+    """
+    if c1 not in df.columns or c2 not in df.columns:
+        raise InvalidColumnException()
+
+    return _pairwise_logratio(df, c1, c2)
--- tests/transformations/coda/pairwise_test.py
+++ tests/transformations/coda/pairwise_test.py
@@ -1,16 +1,18 @@
+import numpy as np
+import pandas as pd
 import pytest
 
 from eis_toolkit.exceptions import InvalidParameterValueException
-from eis_toolkit.transformations.coda.pairwise import single_pairwise_logratio
+from eis_toolkit.transformations.coda.pairwise import pairwise_logratio, single_pairwise_logratio
 
 
-def testsingle_pairwise_logratio():
+def test_single_pairwise_logratio():
     """Test the pairwise logratio transform core functionality."""
     assert single_pairwise_logratio(1.0, 1.0) == 0
     assert single_pairwise_logratio(80.0, 15.0) == pytest.approx(1.67, abs=1e-2)
 
 
-def testsingle_pairwise_logratio_with_zeros():
+def test_single_pairwise_logratio_with_zeros():
     """Test that calling the function with a zero value as either value raises the correct exception."""
     with pytest.raises(InvalidParameterValueException):
         single_pairwise_logratio(0.0, 1.0)
@@ -20,3 +22,12 @@ def testsingle_pairwise_logratio_with_zeros():
 
     with pytest.raises(InvalidParameterValueException):
         single_pairwise_logratio(0.0, 0.0)
+
+
+def test_pairwise_logratio():
+    """Test the pairwise logratio transform core functionality."""
+    arr = np.array([[80, 15, 5], [75, 18, 7]])
+    df = pd.DataFrame(arr, columns=["a", "b", "c"])
+    result = pairwise_logratio(df, "a", "b")
+    assert result[0] == pytest.approx(1.67, abs=1e-2)
+    assert result[1] == pytest.approx(1.43, abs=1e-2)
