0b108a7b1c0ad009bc66b4338dc77621abce0049
Tomi Turunen <tomi.turunen@gtk.fi>
2023-11-30 12:05:47 +0200

    Pre-commit fixes

--- tests/vector_processing/test_calculate_geometry.py
+++ tests/vector_processing/test_calculate_geometry.py
@@ -1,11 +1,11 @@
 import geopandas as gpd
-import pytest
-from shapely.geometry import LineString, Polygon, Point
 import numpy as np
+import pytest
+from shapely.geometry import LineString, Point, Polygon
 
 from eis_toolkit.exceptions import EmptyDataFrameException
-
 from eis_toolkit.vector_processing.calculate_geometry import calculate_geometry
+
 
 @pytest.fixture
 def example_geometries():
@@ -14,24 +14,27 @@ def example_geometries():
     line = LineString([(0, 0), (0, 150)])
     polygon = Polygon([(0, 0), (10, 0), (10, 25), (0, 25)])
 
-    data = {'geometry': [point, line, polygon]}
-    
-    return gpd.GeoDataFrame(data, geometry='geometry')
+    data = {"geometry": [point, line, polygon]}
+
+    return gpd.GeoDataFrame(data, geometry="geometry")
+
 
 def test_validated_calculate_geometries(example_geometries):
     """Test function works with valid data."""
     result = calculate_geometry(example_geometries)
-    
+
     expected_values = np.array([0, 150, 250])
-    calculated_values = result['value'].values
+    calculated_values = result["value"].values
 
     assert np.array_equal(calculated_values, expected_values), "Expected and calculated values do not match"
+
 
 @pytest.fixture
 def empty_gdf():
     """Test data empty gdf."""
     return gpd.GeoDataFrame()
-    
+
+
 def test_empty_geodataframe():
     """Test empty gdf raises an exception."""
     with pytest.raises(EmptyDataFrameException):
