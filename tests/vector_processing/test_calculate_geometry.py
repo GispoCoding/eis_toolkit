672a8d1f783b5149946563a200f2ec85dc970562
Tomi Turunen <tomi.turunen@gtk.fi>
2023-12-12 12:01:37 +0200

    pre-commit fixes

--- tests/vector_processing/test_calculate_geometry.py
+++ tests/vector_processing/test_calculate_geometry.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 import numpy as np
 import pytest
-from shapely.geometry import LineString, Point, Polygon, MultiPoint, MultiLineString, MultiPolygon
+from shapely.geometry import LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon
 
 from eis_toolkit.exceptions import EmptyDataFrameException
 from eis_toolkit.vector_processing.calculate_geometry import calculate_geometry
@@ -41,17 +41,21 @@ def test_empty_geodataframe(empty_gdf):
     with pytest.raises(EmptyDataFrameException):
         calculate_geometry(empty_gdf)
 
+
 @pytest.fixture
 def example_geometries_multi():
     """Test data, MultiPoint, MultiLineString, MultiPolygon."""
-    
+
     multi_point = MultiPoint([(15, 15), (20, 20)])
     multi_line = MultiLineString([[(0, 0), (0, 10)], [(0, 10), (10, 10)]])
-    multi_polygon = MultiPolygon([Polygon([(0, 0), (10, 0), (10, 5), (0, 5)]), Polygon([(0, 5), (10, 5), (10, 10), (0, 10)])])
+    multi_polygon = MultiPolygon(
+        [Polygon([(0, 0), (10, 0), (10, 5), (0, 5)]), Polygon([(0, 5), (10, 5), (10, 10), (0, 10)])]
+    )
 
     data = {"geometry": [multi_point, multi_line, multi_polygon]}
 
     return gpd.GeoDataFrame(data, geometry="geometry")
+
 
 def test_validated_calculate_geometries_multi(example_geometries_multi):
     """Test function works with valid data."""
