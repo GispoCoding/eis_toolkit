03ffd64ad6d674fea299512d43f1b7aa5274bf50
Luca <zelioli.luca@gmail.com>
2023-12-18 17:12:57 +0200

    added the new data (very small npy arrays)

--- tests/prediction/cnn_classification_and_regression_test.py
+++ tests/prediction/cnn_classification_and_regression_test.py
@@ -1,5 +1,3 @@
-import os
-
 import numpy as np
 
 from eis_toolkit.prediction.cnn_classification_and_regression import (
@@ -7,26 +5,12 @@ from eis_toolkit.prediction.cnn_classification_and_regression import (
     train_and_predict_for_regression,
 )
 
-PATH = "tests/prediction/Windows_test"
-
-dataset = list()
-labels = list()
-
-for windows in os.listdir(PATH):
-    path = f"{PATH}/{windows}"
-    for folder in os.listdir(f"{path}"):
-        for single_windows in os.listdir(f"{path}/{folder}"):
-            full_path = f"{path}/{folder}/{single_windows}"
-            dataset.append(np.load(full_path))
-            temp = full_path.split("_")[-1].split(".")[0]
-            labels.append(int(temp))
-
-X = np.array(dataset)
-y = np.array(labels)
-
 
 def test_do_classification():
     """Test for classification."""
+    X = np.load("data/data.npy")
+    y = np.load("data/labels.npy")
+
     best_model, cm = train_and_predict_for_classification(
         X=X,
         y=y,
@@ -46,6 +30,8 @@ def test_do_classification():
 
 def test_do_regression():
     """Test for regression."""
+    X = np.load("data/data.npy")
+    y = np.load("data/labels.npy")
     best_model, cm = train_and_predict_for_regression(
         X=X,
         y=y,
