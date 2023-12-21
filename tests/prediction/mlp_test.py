40071b6479639b1292431aa434ee34130dbb3803
Niko Aarnio <niko.aarnio@protonmail.com>
2023-12-21 13:15:54 +0200

    Test new exceptions, test exact scores using seed

--- tests/prediction/mlp_test.py
+++ tests/prediction/mlp_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from sklearn.datasets import load_diabetes, load_iris
 from sklearn.model_selection import train_test_split
@@ -9,13 +10,14 @@ from eis_toolkit.transformations.one_hot_encoding import one_hot_encode
 
 X_IRIS, Y_IRIS = load_iris(return_X_y=True)
 X_DIABETES, Y_DIABETES = load_diabetes(return_X_y=True)
+SEED = 42
 
 
 def test_train_MLP_classifier():
     """Test that training MLP classifier works as expected."""
     X = StandardScaler().fit_transform(X_IRIS)
     y = one_hot_encode(Y_IRIS).toarray()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
 
     model, history = train_MLP_classifier(
         X_train,
@@ -25,6 +27,7 @@ def test_train_MLP_classifier():
         last_activation="softmax",
         loss_function="categorical_crossentropy",
         metrics=["accuracy"],
+        random_state=SEED,
     )
 
     assert model is not None, "Model should not be None"
@@ -35,18 +38,19 @@ def test_train_MLP_classifier():
     scores = model.evaluate(X_test, y_test, verbose=0)
     test_accuracy = scores[1]
 
-    # Can't check if output is equal to a single number as there is some randomness involved in creating MLP model
-    assert history["accuracy"][-1] > 0.7, "Last epoch's accuracy should be reasonably high"
-    assert test_accuracy > 0.7, "Test accuracy should be reasonably high"
+    np.testing.assert_almost_equal(history["accuracy"][-1], 0.85417, 4)
+    np.testing.assert_almost_equal(test_accuracy, 0.9000, 4)
 
 
 def test_train_MLP_regressor():
     """Test that training MLP regressor works as expected."""
     X = StandardScaler().fit_transform(X_DIABETES)
     y = one_hot_encode(Y_DIABETES).toarray()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
 
-    model, history = train_MLP_regressor(X_train, y_train, neurons=[16], output_neurons=1, metrics=["mse"])
+    model, history = train_MLP_regressor(
+        X_train, y_train, neurons=[16], output_neurons=1, metrics=["mse"], random_state=SEED
+    )
 
     assert model is not None, "Model should not be None"
     assert "loss" in history, "History should contain loss"
@@ -56,9 +60,22 @@ def test_train_MLP_regressor():
     scores = model.evaluate(X_test, y_test, verbose=0)
     test_mse = scores[1]
 
-    # Can't check if output is equal to a single number as there is some randomness involved in creating MLP model
-    assert history["mse"][-1] < 0.3, "Last epoch's MSE should be reasonably low"
-    assert test_mse < 0.3, "Test MSE should be reasonably low"
+    np.testing.assert_almost_equal(history["mse"][-1], 0.01287, 4)
+    np.testing.assert_almost_equal(test_mse, 0.02062, 4)
+
+
+def test_invalid_X_shape():
+    """Test that invalid shape for X raises the correct exception."""
+    with pytest.raises(exceptions.InvalidDataShapeException):
+        train_MLP_classifier(X_IRIS.ravel(), Y_IRIS, neurons=[])
+
+
+def test_nonmatching_sample_count():
+    """Test that differng number of samples for X and y raises the correct exception."""
+    extra_row = np.random.rand(1, X_IRIS.shape[1])
+    X_mismatched = np.concatenate((X_IRIS, extra_row), axis=0)
+    with pytest.raises(exceptions.InvalidDataShapeException):
+        train_MLP_classifier(X_mismatched, Y_IRIS, neurons=[])
 
 
 def test_invalid_neurons():
@@ -129,3 +146,9 @@ def test_invalid_output_neurons_for_binary_crossentropy():
     """Test that invalid value for output_neurons for binary crossentropy raises the correct exception."""
     with pytest.raises(exceptions.InvalidParameterValueException):
         train_MLP_classifier(X_IRIS, Y_IRIS, neurons=[16], output_neurons=2, loss_function="binary_crossentropy")
+
+
+def test_invalid_output_neurons_for_categorical_crossentropy():
+    """Test that invalid value for output_neurons for categorical crossentropy raises the correct exception."""
+    with pytest.raises(exceptions.InvalidParameterValueException):
+        train_MLP_classifier(X_IRIS, Y_IRIS, neurons=[16], output_neurons=1, loss_function="categorical_crossentropy")
