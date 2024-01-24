5e486291b60b6afa8cb356cefd3ba7ed75a190ed
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2024-01-24 10:11:38 +0200

    Change reclassify raster tests to use private functions, assert return types

--- tests/raster_processing/reclassify_raster_test.py
+++ tests/raster_processing/reclassify_raster_test.py
@@ -1,159 +1,109 @@
-import mapclassify as mc
 import numpy as np
+import pytest
+import rasterio
+from beartype.typing import Sequence
+
+from eis_toolkit.exceptions import InvalidParameterValueException
+from eis_toolkit.raster_processing import reclassify_raster
+from tests.raster_processing.clip_test import raster_path as SMALL_RASTER_PATH
 
 test_array = np.array([[0, 10, 20, 30], [40, 50, 50, 60], [80, 80, 90, 90], [100, 100, 100, 100]])
 
 
 def test_raster_with_defined_intervals():
-    """Test raster with defined intervals by comparing the output of the function to numpy's digitized result."""
+    """Test raster with defined intervals."""
     interval_size = 3
 
-    _, edges = np.histogram(test_array, bins=interval_size)
-
-    data = np.digitize(test_array, edges)
+    result = reclassify_raster._raster_with_defined_intervals(test_array, interval_size)
 
     expected_output = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
 
-    np.testing.assert_allclose(data, expected_output)
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
+
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def raster_with_equal_intervals():
-    """Test raster with equal intervals by comparing the output to numpy's digitized result."""
+    """Test raster with equal intervals."""
     number_of_intervals = 10
 
-    expected_intervals = np.linspace(0, 100, number_of_intervals)
-    data = np.digitize(test_array, expected_intervals)
+    result = reclassify_raster._raster_with_equal_intervals(test_array, number_of_intervals)
 
     expected_output = np.array([[1, 2, 3, 4], [5, 6, 6, 7], [9, 9, 10, 10], [11, 11, 11, 11]])
 
-    np.testing.assert_allclose(data, expected_output)
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
+
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def test_raster_with_geometrical_intervals():
-    """Test raster with geometrical intervals by comparing the output of the function to the original data."""
+    """Test raster with geometrical intervals."""
     number_of_classes = 10
+    nan_value = -9999
 
-    median_value = np.nanmedian(test_array)
-    max_value = np.nanmax(test_array)
-    min_value = np.nanmin(test_array)
-    print(median_value)
+    result = reclassify_raster._raster_with_geometrical_intervals(test_array, number_of_classes, nan_value)
 
-    test_array_flat = test_array.flatten()
-    test_array_flat = np.ma.masked_where(np.isnan(test_array_flat), test_array_flat)
-
-    values_out = np.zeros_like(test_array_flat)  # The same shape as the flattened array
-
-    if (median_value - min_value) < (max_value - median_value):  # Large end tail longer
-        raster_half = test_array_flat[np.where((test_array_flat > median_value) & (test_array_flat != np.nan))]
-        range_half = max_value - median_value
-        raster_half = raster_half - median_value + (range_half) / 1000.0
-    else:  # Small end tail longer
-        raster_half = test_array_flat[np.where((test_array_flat < median_value) & (test_array_flat != np.nan))]
-        range_half = median_value - min_value
-        raster_half = raster_half - min_value + (range_half) / 1000.0
-
-    min_half = np.nanmin(raster_half)
-    max_half = np.nanmax(raster_half)
-
-    # Number of classes
-    fac = (max_half / min_half) ** (1 / number_of_classes)
-
-    ibp = 1
-    brpt_half = [min_half]
-    brpt = [min_half]
-    width = [0]
-
-    while brpt[-1] < max_half:
-        ibp += 1
-        brpt.append(min_half * fac ** (ibp - 1))
-        brpt_half.append(brpt[-1])
-        width.append(brpt_half[-1] - brpt_half[0])
-
-    k = 0
-
-    for j in range(1, len(width) - 2):
-        values_out[
-            np.where(
-                ((median_value + width[j]) < test_array_flat)
-                & (test_array_flat <= (median_value + width[j + 1]))
-                & (test_array_flat != np.nan)
-            )
-        ] = (j + 1)
-        values_out[
-            np.where(
-                ((median_value - width[j]) > test_array_flat)
-                & (test_array_flat >= (median_value - width[j + 1]))
-                & (test_array_flat != np.nan)
-            )
-        ] = (-j - 1)
-        k = j
-
-    values_out[np.where(((median_value + width[k + 1]) < test_array_flat) & (test_array_flat != np.nan))] = k + 1
-    values_out[np.where(((median_value - width[k + 1]) > test_array_flat) & (test_array_flat != np.nan))] = -k - 1
-    values_out[np.where(median_value == test_array_flat)] = 0
-
-    values_out = values_out.reshape(test_array.shape)
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
 
     expected_output = np.array([[-9, -9, -9, -9], [-9, -9, -9, -8], [8, 8, 9, 9], [9, 9, 9, 9]])
 
-    np.testing.assert_allclose(values_out, expected_output)
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def test_raster_with_manual_breaks():
-    """Test raster with manual break intervals by comparing the output of the function to numpy's digitized result."""
+    """Test raster with manual break intervals."""
     breaks = [20, 40, 60, 80]
 
-    data = np.digitize(test_array, breaks)
+    result = reclassify_raster._raster_with_manual_breaks(test_array, breaks)
+
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
 
     expected_output = np.array([[0, 0, 1, 1], [2, 2, 2, 3], [4, 4, 4, 4], [4, 4, 4, 4]])
 
-    np.testing.assert_allclose(data, expected_output)
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def test_raster_with_natural_breaks():
-    """Test raster with natural breaks by comparing the output of the function to MapClassify's Jenks Caspall."""
+    """Test raster with natural breaks."""
     number_of_classes = 10
 
-    breaks = mc.JenksCaspall(test_array, number_of_classes)
-    data = np.digitize(test_array, np.sort(breaks.bins))
+    result = reclassify_raster._raster_with_natural_breaks(test_array, number_of_classes)
+
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
 
     expected_output = np.array([[0, 1, 1, 2], [3, 4, 4, 5], [6, 6, 7, 7], [8, 8, 8, 8]])
 
-    np.testing.assert_allclose(data, expected_output)
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def test_raster_with_standard_deviation():
-    """Test raster with standard deviation intervals by comparing the output of the function to the original data."""
+    """Test raster with standard deviation intervals."""
     number_of_intervals = 75
 
-    stddev = np.nanstd(test_array)
-    mean = np.nanmean(test_array)
-    interval_size = 2 * stddev / number_of_intervals
+    result = reclassify_raster._raster_with_standard_deviation(test_array, number_of_intervals)
 
-    classified = np.empty_like(test_array)
-
-    below_mean = test_array < (mean - stddev)
-    above_mean = test_array > (mean + stddev)
-
-    classified[below_mean] = -number_of_intervals
-    classified[above_mean] = number_of_intervals
-
-    in_between = ~below_mean & ~above_mean
-    interval = ((test_array - (mean - stddev)) / interval_size).astype(int)
-    classified[in_between] = interval[in_between] - number_of_intervals // 2
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
 
     expected_output = np.array([[-75, -75, -75, -36], [-25, -14, -14, -3], [20, 20, 31, 31], [75, 75, 75, 75]])
 
-    np.testing.assert_allclose(classified, expected_output)
+    np.testing.assert_allclose(result[0], expected_output)
 
 
 def test_raster_with_quantiles():
-    """Test raster with quantile intervals by comparing the output of the function to the original data."""
+    """Test raster with quantile intervals by."""
     number_of_quantiles = 4
 
-    intervals = [np.percentile(test_array, i * 100 / number_of_quantiles) for i in range(number_of_quantiles)]
-    data = np.digitize(test_array, intervals)
+    result = reclassify_raster._raster_with_quantiles(test_array, number_of_quantiles)
+
+    assert isinstance(result, Sequence)
+    assert isinstance(result[0], np.ndarray)
 
     expected_output = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
 
-    np.testing.assert_allclose(data, expected_output)
+    np.testing.assert_allclose(result[0], expected_output)
