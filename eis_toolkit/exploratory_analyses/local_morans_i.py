d5622c4a1e5a685418005c75712a7e42a39e72e6
Tomi Turunen <tomi.turunen@gtk.fi>
2024-01-25 08:46:57 +0200

    Minor changes

--- eis_toolkit/exploratory_analyses/local_morans_i.py
+++ eis_toolkit/exploratory_analyses/local_morans_i.py
@@ -1,23 +1,20 @@
-import numpy as np
-from esda.moran import Moran_Local
-import libpysal
 import geopandas as gpd
+import libpysal
+import numpy as np
 from beartype import beartype
 from beartype.typing import Literal
+from esda.moran import Moran_Local
 
 from eis_toolkit import exceptions
 
+
 @beartype
 def _local_morans_i(
-    gdf: gpd.GeoDataFrame,
-    column: str,
-    weight_type: Literal["queen", "knn"],
-    k: int,
-    permutations: int
+    gdf: gpd.GeoDataFrame, column: str, weight_type: Literal["queen", "knn"], k: int, permutations: int
 ) -> gpd.GeoDataFrame:
 
-    gdf['lon'] = gdf['geometry'].x
-    gdf['lat'] = gdf['geometry'].y
+    gdf["lon"] = gdf["geometry"].x
+    gdf["lat"] = gdf["geometry"].y
 
     if weight_type == "queen":
         w = libpysal.weights.Queen.from_dataframe(gdf)
@@ -27,10 +24,10 @@ def _local_morans_i(
         raise ValueError("Invalid weight_type. Use 'queen' or 'knn'.")
 
     if len(gdf[column]) != len(w.weights):
-        raise ValueError('Dimension mismatch between data and weights matrix.')
-    
+        raise ValueError("Dimension mismatch between data and weights matrix.")
+
     moran_loc = Moran_Local(gdf[column], w, permutations=permutations)
-    
+
     gdf[f"{column}_local_moran_I"] = moran_loc.Is
     gdf[f"{column}_p_value"] = moran_loc.p_sim
 
@@ -38,13 +35,14 @@ def _local_morans_i(
 
     return gdf
 
+
 @beartype
 def local_morans_i(
     gdf: gpd.GeoDataFrame,
     column: str,
     weight_type: Literal["queen", "knn"] = "queen",
     k: int = 2,
-    permutations: int = 999
+    permutations: int = 999,
 ) -> gpd.GeoDataFrame:
     """Do a local morans I calculation for the area.
 
