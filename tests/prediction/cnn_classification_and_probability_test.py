c1f0bf88341174a608aeb033eede95ec105451b1
Luca <zelioli.luca@gmail.com>
2024-02-13 12:16:35 +0200

    added the some corrections

--- tests/prediction/cnn_classification_and_probability_test.py
+++ tests/prediction/cnn_classification_and_probability_test.py
@@ -2,22 +2,21 @@ import os
 
 import numpy as np
 import pytest
-from sklearn.metrics import confusion_matrix
+import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
-from eis_toolkit.exceptions import InvalidArgumentTypeException, InvalidParameterValueException
+from eis_toolkit.exceptions import InvalidArgumentTypeException
 from eis_toolkit.prediction.cnn_classification_and_probability import (
-    train_and_predict_for_classification,
-    train_and_predict_for_regression,
+    run_inference_for_classification,
+    run_inference_for_regression,
 )
 from eis_toolkit.prediction.model_performance_estimation import performance_model_estimation
 from eis_toolkit.transformations.normalize_data import normalize_the_data
 from eis_toolkit.transformations.one_hot_encoding import one_hot_encode
 
 
-def test_do_the_classification():
+def test_train_CNN_classifier_with_categorical_crossentropy():
     """Test for classification."""
-    print(f"{os.getcwd()}")
     data = np.load(f'{os.path.join("data", "data.npy")}')
     labels = np.load(f'{os.path.join("data", "labels.npy")}')
 
@@ -41,38 +40,36 @@ def test_do_the_classification():
         x_validation = normalize_the_data(scaler_agent=scaler_agent, data=data[validation_idx])
         y_validation = encoded_labels[validation_idx]
 
-        cnn_model, true_labels, predicted_labels, score = train_and_predict_for_classification(
-            x_train=x_train,
-            y_train=y_train,
-            x_validation=x_validation,
-            y_validation=y_validation,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=x_train,
+            y=y_train,
+            validation_split=0.2,
+            validation_data=(x_validation, y_validation),
             batch_size=32,
             epochs=10,
             conv_list=[4, 8],
             neuron_list=[8],
             input_shape_for_cnn=(x_train.shape[1], x_train.shape[2], x_train.shape[3]),
             convolutional_kernel_size=(x_train.shape[3], x_train.shape[3]),
+            last_activation_layer="softmax",
+            loss_function=tf.keras.losses.CategoricalCrossentropy(),
         )
 
         if stacked_true is None:
-            stacked_true = np.argmax(true_labels, axis=1)
+            stacked_true = np.argmax(y_validation, axis=1)
         else:
-            stacked_true = np.concatenate((stacked_true, np.argmax(true_labels, axis=1)))
+            stacked_true = np.concatenate((stacked_true, np.argmax(y_validation, axis=1)))
 
         if stacked_predicted is None:
             stacked_predicted = np.argmax(predicted_labels, axis=1)
         else:
             stacked_predicted = np.concatenate((stacked_predicted, np.argmax(predicted_labels, axis=1)))
 
-    # make cm
-    cm = confusion_matrix(stacked_true, stacked_predicted)
-    print(cm)
-    assert cm.shape[0] != 0 and cm.shape[1] != 0
+    assert stacked_true.shape != 0 and stacked_predicted.shape != 0
 
 
-def test_do_the_regression():
-    """Test for classification."""
-    print(f"{os.getcwd()}")
+def test_train_CNN_classifier_with_binary_crossentropy():
+    """Test for classification with binary cross entropy."""
     data = np.load(f'{os.path.join("data", "data.npy")}')
     labels = np.load(f'{os.path.join("data", "labels.npy")}')
 
@@ -93,11 +90,63 @@ def test_do_the_regression():
         x_validation = normalize_the_data(scaler_agent=scaler_agent, data=data[validation_idx])
         y_validation = labels[validation_idx]
 
-        cnn_model, true_labels, predicted_labels, probabilities, score = train_and_predict_for_regression(
-            x_train=x_train,
-            y_train=y_train,
-            x_validation=x_validation,
-            y_validation=y_validation,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=x_train,
+            y=y_train,
+            validation_split=0.2,
+            validation_data=(x_validation, y_validation),
+            batch_size=32,
+            epochs=10,
+            conv_list=[4, 8],
+            neuron_list=[8],
+            input_shape_for_cnn=(x_train.shape[1], x_train.shape[2], x_train.shape[3]),
+            convolutional_kernel_size=(x_train.shape[3], x_train.shape[3]),
+            last_activation_layer="sigmoid",
+            loss_function=tf.keras.losses.BinaryCrossentropy(),
+            output_units=1,
+        )
+
+        if stacked_true is None:
+            stacked_true = y_validation
+        else:
+            stacked_true = np.concatenate((stacked_true, y_validation))
+
+        if stacked_predicted is None:
+
+            stacked_predicted = (predicted_labels >= 0.5).astype(int)
+        else:
+            stacked_predicted = np.concatenate((stacked_predicted, (predicted_labels >= 0.5).astype(int)))
+
+    assert stacked_true.shape != 0 and stacked_predicted.shape != 0
+
+
+def test_train_CNN_regressor():
+    """Test for classification."""
+    data = np.load(f'{os.path.join("data", "data.npy")}')
+    labels = np.load(f'{os.path.join("data", "labels.npy")}')
+
+    # create a scaler agent
+    scaler_agent = StandardScaler()
+    scaler_agent.fit(data.reshape(-1, data.shape[-1]))
+
+    # make cv
+    selected_cv = performance_model_estimation(cross_validation_type="SKFOLD", number_of_split=5)
+
+    stacked_true, stacked_predicted = None, None
+
+    for i, (train_idx, validation_idx) in enumerate(selected_cv.split(data, labels)):
+
+        x_train = normalize_the_data(scaler_agent=scaler_agent, data=data[train_idx])
+        y_train = labels[train_idx]
+
+        x_validation = normalize_the_data(scaler_agent=scaler_agent, data=data[validation_idx])
+        y_validation = labels[validation_idx]
+
+        cnn_model, predicted_labels, probabilities, score = run_inference_for_regression(
+            X=x_train,
+            y=y_train,
+            validation_split=0.2,
+            validation_data=(x_validation, y_validation),
             batch_size=32,
             epochs=10,
             conv_list=[4, 8],
@@ -105,24 +154,21 @@ def test_do_the_regression():
             input_shape_for_cnn=(x_train.shape[1], x_train.shape[2], x_train.shape[3]),
             convolutional_kernel_size=(x_train.shape[3], x_train.shape[3]),
             threshold=0.5,
+            last_activation_layer=None,
+            loss_function=tf.keras.losses.MeanAbsoluteError(),
+            output_units=1,
         )
 
         if stacked_true is None:
-            stacked_true = true_labels
-            print(stacked_true.shape)
+            stacked_true = y_validation
         else:
-            stacked_true = np.concatenate((stacked_true, true_labels))
-
+            stacked_true = np.concatenate((stacked_true, y_validation))
         if stacked_predicted is None:
             stacked_predicted = predicted_labels
-            print(predicted_labels.shape)
         else:
             stacked_predicted = np.concatenate((stacked_predicted, predicted_labels))
 
-    # make cm
-    cm = confusion_matrix(stacked_true, stacked_predicted)
-    print(cm)
-    assert cm.shape[0] != 0 and cm.shape[1] != 0
+    assert stacked_true.shape[0] != 0 and stacked_predicted.shape[0] != 0
 
 
 def test_invalid_convolutional_layer():
@@ -131,11 +177,11 @@ def test_invalid_convolutional_layer():
         x_train = np.load(f'{os.path.join("data", "data.npy")}')
         y_train = np.load(f'{os.path.join("data", "labels.npy")}')
 
-        cnn_model, true_labels, predicted_labels, score = train_and_predict_for_classification(
-            x_train=x_train,
-            y_train=y_train,
-            x_validation=None,
-            y_validation=None,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=x_train,
+            y=y_train,
+            validation_split=0.2,
+            validation_data=None,
             batch_size=32,
             epochs=10,
             conv_list=[],
@@ -151,11 +197,11 @@ def test_invalid_neurons_layer():
         x_train = np.load(f'{os.path.join("data", "data.npy")}')
         y_train = np.load(f'{os.path.join("data", "labels.npy")}')
 
-        cnn_model, true_labels, predicted_labels, score = train_and_predict_for_classification(
-            x_train=x_train,
-            y_train=y_train,
-            x_validation=None,
-            y_validation=None,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=x_train,
+            y=y_train,
+            validation_split=0.2,
+            validation_data=None,
             batch_size=32,
             epochs=10,
             conv_list=[8],
@@ -167,15 +213,15 @@ def test_invalid_neurons_layer():
 
 def test_invalid_parameters_dropout_exception():
     """Invalid dropout test."""
-    with pytest.raises(InvalidParameterValueException):
+    with pytest.raises(InvalidArgumentTypeException):
         x_train = np.load(f'{os.path.join("data", "data.npy")}')
         y_train = np.load(f'{os.path.join("data", "labels.npy")}')
 
-        cnn_model, true_labels, predicted_labels, score = train_and_predict_for_classification(
-            x_train=x_train,
-            y_train=y_train,
-            x_validation=None,
-            y_validation=None,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=x_train,
+            y=y_train,
+            validation_split=None,
+            validation_data=None,
             batch_size=32,
             epochs=10,
             conv_list=[8],
@@ -191,11 +237,11 @@ def test_invalid_parameters_inputs_exception():
     with pytest.raises(InvalidArgumentTypeException):
         x_train = np.load(f'{os.path.join("data", "data.npy")}')
 
-        cnn_model, true_labels, predicted_labels, score = train_and_predict_for_classification(
-            x_train=np.array([]),
-            y_train=np.array([]),
-            x_validation=None,
-            y_validation=None,
+        cnn_model, predicted_labels, score = run_inference_for_classification(
+            X=np.array([]),
+            y=np.array([]),
+            validation_split=None,
+            validation_data=None,
             batch_size=32,
             epochs=10,
             conv_list=[8],
