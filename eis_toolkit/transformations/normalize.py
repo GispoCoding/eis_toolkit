0ad6f0bdc267493c97735cf9561fa4aae6acd2a2
Niko Aarnio <niko.aarnio@protonmail.com>
2024-03-01 12:57:23 +0200

    fix(transformations): standardization and normalization now return copy of data, have invalid col check, force float dtype have better docs, standardization uses ddof=0

--- eis_toolkit/transformations/normalize.py
+++ eis_toolkit/transformations/normalize.py
@@ -3,7 +3,7 @@ import pandas as pd
 from beartype import beartype
 from beartype.typing import Literal, Optional, Sequence, Union
 
-from eis_toolkit.exceptions import InvalidDataShapeException, InvalidParameterValueException
+from eis_toolkit.exceptions import InvalidColumnException, InvalidDataShapeException, InvalidParameterValueException
 
 
 @beartype
@@ -13,6 +13,8 @@ def normalize(
     columns: Optional[Sequence[str]] = None,
 ) -> Union[np.ndarray, pd.DataFrame]:
     """Normalize input data.
+
+    Scales values to range [0,1].
 
     Normalization is applied to each variable independently. For DataFrames, each column is
     treated as a variable and for Numpy arrays, either each column or each 2D array is treated
@@ -32,24 +34,29 @@ def normalize(
         InvalidParameterValueException: If array type selection is invalid.
         InvalidDataShapeException: If shape of Numpy array is invalid for selected array type.
     """
+    out_data = data.copy().astype(np.float64)
     if isinstance(data, pd.DataFrame):
         if columns is None:
             columns = data.columns
         for col in columns:
-            data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
+            if col not in data.columns:
+                raise InvalidColumnException(f"Column {col} was not found in the input DataFrame.")
+            out_data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
     else:
         if array_type == "tabular":
             if data.ndim != 2:
                 raise InvalidDataShapeException("Tabular data must be a 2D numpy array.")
-            data = (data - data.min(axis=0)) / (data.max(axis=0) - data.min(axis=0))
+            out_data = (data - data.min(axis=0)) / (data.max(axis=0) - data.min(axis=0))
         elif array_type == "raster":
             if data.ndim == 2:  # Treat like a single-band raster
-                data = (data - data.min()) / (data.max() - data.min())
+                out_data = (data - data.min()) / (data.max() - data.min())
             elif data.ndim == 3:
                 for i in range(data.shape[0]):
-                    data[i, :, :] = (data[i, :, :] - data[i, :, :].min()) / (data[i, :, :].max() - data[i, :, :].min())
+                    min = data[i, :, :].min()
+                    max = data[i, :, :].max()
+                    out_data[i] = (data[i] - min) / (max - min)
             else:
                 raise InvalidDataShapeException("Raster data must be a 2D or 3D numpy array.")
         else:
             raise InvalidParameterValueException("data_type must be either 'tabular' or 'raster'.")
-    return data
+    return out_data
--- eis_toolkit/transformations/standardize.py
+++ eis_toolkit/transformations/standardize.py
@@ -3,7 +3,7 @@ import pandas as pd
 from beartype import beartype
 from beartype.typing import Literal, Optional, Sequence, Union
 
-from eis_toolkit.exceptions import InvalidDataShapeException, InvalidParameterValueException
+from eis_toolkit.exceptions import InvalidColumnException, InvalidDataShapeException, InvalidParameterValueException
 
 
 @beartype
@@ -13,6 +13,8 @@ def standardize(
     columns: Optional[Sequence[str]] = None,
 ) -> Union[np.ndarray, pd.DataFrame]:
     """Standardize input data.
+
+    Scales data mean of 0 and standard deviation of 1.
 
     Standardization is applied to each variable independently. For DataFrames, each column is
     treated as a variable and for Numpy arrays, either each column or each 2D array is treated
@@ -32,24 +34,27 @@ def standardize(
         InvalidParameterValueException: If array type selection is invalid.
         InvalidDataShapeException: If shape of Numpy array is invalid for selected array type.
     """
+    out_data = data.copy().astype(np.float64)
     if isinstance(data, pd.DataFrame):
         if columns is None:
             columns = data.columns
         for col in columns:
-            data[col] = (data[col] - data[col].mean()) / data[col].std()
+            if col not in data.columns:
+                raise InvalidColumnException(f"Column {col} was not found in the input DataFrame.")
+            out_data[col] = (data[col] - data[col].mean()) / data[col].std(ddof=0)
     else:
         if array_type == "tabular":
             if data.ndim != 2:
                 raise InvalidDataShapeException("Tabular data must be a 2D numpy array.")
-            data = (data - data.mean(axis=0)) / data.std(axis=0)
+            out_data = (data - data.mean(axis=0)) / data.std(axis=0, ddof=0)
         elif array_type == "raster":
             if data.ndim == 2:  # Treat like a single-band raster
-                data = (data - data.mean()) / data.std()
+                out_data = (data - data.mean()) / data.std(ddof=0)
             elif data.ndim == 3:
                 for i in range(data.shape[0]):
-                    data[i, :, :] = (data[i, :, :] - data[i, :, :].mean()) / data[i, :, :].std()
+                    out_data[i, :, :] = (data[i, :, :] - data[i, :, :].mean()) / data[i, :, :].std(ddof=0)
             else:
                 raise InvalidDataShapeException("Raster data must be a 2D or 3D numpy array.")
         else:
             raise InvalidParameterValueException("array_type must be either 'tabular' or 'raster'.")
-    return data
+    return out_data
