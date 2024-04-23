f9dd2a1af530ea8a1cffa775aaca21b16046b602
Niko Aarnio <niko.aarnio@protonmail.com>
2024-04-23 11:49:58 +0300

    refactor(IDW): changed IDW to take in raster profile

--- eis_toolkit/vector_processing/idw_interpolation.py
+++ eis_toolkit/vector_processing/idw_interpolation.py
@@ -3,49 +3,41 @@ from numbers import Number
 import geopandas as gpd
 import numpy as np
 from beartype import beartype
-from beartype.typing import Optional, Tuple
-from rasterio import transform
+from beartype.typing import Union
+from rasterio import profiles, transform
 
-from eis_toolkit.exceptions import EmptyDataFrameException, InvalidParameterValueException
+from eis_toolkit.exceptions import EmptyDataFrameException, InvalidParameterValueException, NonMatchingCrsException
+from eis_toolkit.utilities.checks.raster import check_raster_profile
 
 
 @beartype
 def _idw_interpolation(
     geodataframe: gpd.GeoDataFrame,
     target_column: str,
-    resolution: Tuple[Number, Number],
+    raster_width: int,
+    raster_height: int,
+    raster_transform: transform.Affine,
     power: Number,
-    extent: Optional[Tuple[Number, Number, Number, Number]],
-) -> Tuple[np.ndarray, dict]:
+) -> np.ndarray:
 
     points = np.array(geodataframe.geometry.apply(lambda geom: (geom.x, geom.y)).tolist())
     values = geodataframe[target_column].values
 
-    if extent is None:
-        x_min, y_min, x_max, y_max = geodataframe.geometry.total_bounds
-    else:
-        x_min, x_max, y_min, y_max = extent
+    pixel_size_x = raster_transform.a
+    pixel_size_y = abs(raster_transform.e)
+    grid_x_min = raster_transform.xoff
+    grid_x_max = grid_x_min + raster_width * pixel_size_x
+    grid_y_max = raster_transform.yoff
+    grid_y_min = grid_y_max - raster_height * pixel_size_y
 
-    resolution_x, resolution_y = resolution
-
-    num_points_x = int((x_max - x_min) / resolution_x)
-    num_points_y = int((y_max - y_min) / resolution_y)
-
-    x = np.linspace(x_min, x_max, num_points_x)
-    y = np.linspace(y_min, y_max, num_points_y)
+    x = np.linspace(grid_x_min, grid_x_max, raster_width)
+    y = np.linspace(grid_y_min, grid_y_max, raster_height)
     y = y[::-1].reshape(-1, 1)
 
     interpolated_values = _idw_core(points[:, 0], points[:, 1], values, x, y, power)
-    interpolated_values = interpolated_values.reshape(num_points_y, num_points_x)
+    interpolated_values = interpolated_values.reshape(raster_height, raster_width)
 
-    out_meta = {
-        "crs": geodataframe.crs,
-        "width": len(x),
-        "height": len(y),
-        "transform": transform.from_bounds(x_min, y_min, x_max, y_max, len(x), len(y)),
-    }
-
-    return interpolated_values, out_meta
+    return interpolated_values
 
 
 #  Distance calculations
@@ -69,41 +61,43 @@ def _idw_core(x, y, z, xi, yi: np.ndarray, power: Number) -> np.ndarray:
 def idw(
     geodataframe: gpd.GeoDataFrame,
     target_column: str,
-    resolution: Tuple[Number, Number],
-    extent: Optional[Tuple[Number, Number, Number, Number]] = None,
+    raster_profile: Union[profiles.Profile, dict],
     power: Number = 2,
-) -> Tuple[np.ndarray, dict]:
+) -> np.ndarray:
     """Calculate inverse distance weighted (IDW) interpolation.
 
     Args:
         geodataframe: The vector dataframe to be interpolated.
         target_column: The column name with values for each geometry.
-        resolution: The resolution i.e. cell size of the output raster as (pixel_size_x, pixel_size_y).
-        extent: The extent of the output raster as (x_min, x_max, y_min, y_max).
-            If None, calculate extent from the input vector data.
-        power: The value for determining the rate at which the weights decrease.
-            As power increases, the weights for distant points decrease rapidly.
-            Defaults to 2.
+        raster_profile: The raster profile used for output grid properties. Needs to include at least
+            crs, transform, width and height.
+        power: The value for determining the rate at which the weights decrease. As power increases,
+            the weights for distant points decrease rapidly. Defaults to 2.
 
     Returns:
-        Rasterized vector data and metadata.
+        Numpy array containing the interpolated values.
 
     Raises:
         EmptyDataFrameException: The input GeoDataFrame is empty.
         InvalidParameterValueException: Invalid resolution or target_column.
+        NonMatchingCrsException: The input GeoDataFrame and raster profile have mismatching CRS.
     """
-
-    if geodataframe.shape[0] == 0:
+    if geodataframe.empty:
         raise EmptyDataFrameException("Expected geodataframe to contain geometries.")
-
+    if raster_profile.get("crs") != geodataframe.crs:
+        raise NonMatchingCrsException("Expected coordinate systems to match between raster and GeoDataFrame.")
     if target_column not in geodataframe.columns:
         raise InvalidParameterValueException(
             f"Expected target_column ({target_column}) to be contained in geodataframe columns."
         )
+    check_raster_profile(raster_profile)
 
-    if resolution[0] <= 0 or resolution[1] <= 0:
-        raise InvalidParameterValueException("Expected height and width greater than zero.")
+    raster_width = raster_profile.get("width")
+    raster_height = raster_profile.get("height")
+    raster_transform = raster_profile.get("transform")
 
-    interpolated_values, out_meta = _idw_interpolation(geodataframe, target_column, resolution, power, extent)
+    interpolated_values = _idw_interpolation(
+        geodataframe, target_column, raster_width, raster_height, raster_transform, power
+    )
 
-    return interpolated_values, out_meta
+    return interpolated_values
