9e75fb69d5b193d5e8b23dc2dd82583f467dd7b5
Omkar Kolekar <okolekar123@gmail.com>
2024-10-01 12:26:28 +0200

    The tool now takes in a GeoDataFrame, raster profile and maximum distance as inputs and performs the inversion. Logarithmic scaling is also introduced as result of previous discussions

--- eis_toolkit/vector_processing/proximity_tool.py
+++ eis_toolkit/vector_processing/proximity_tool.py
@@ -1,10 +1,13 @@
 import numpy as np
-from beartype import beartype
-from beartype.typing import  Optional
+import geopandas as gpd
 from numbers import Number
+from beartype import beartype
+from beartype.typing import Union
+from rasterio import profiles, transform
+from eis_toolkit.vector_processing.distance_computation import distance_computation
 
 @beartype
-def calculate_proximity(distance_array: np.ndarray, maximum_distance: Optional[Number] = None) -> np.ndarray:
+def calculate_proximity(geodataframe: gpd.GeoDataFrame, raster_profile: Union[profiles.Profile, dict], maximum_distance: Number) -> np.ndarray:
 
     """ Interpolates the distance values calculated by the distance_computation function between 0 and 1.
         1 denots the value inside the polygon and 0 at the maximum distance.
@@ -13,22 +16,58 @@ def calculate_proximity(distance_array: np.ndarray, maximum_distance: Optional[N
         Uses linear interpolation to calculate the distance from the polygon.
 
         Args:
-            distance_array: The distance array calculated from the distance_computation
-            maximum_distance: The maximum distance from the polygon
+            geodataframe: The GeoDataFrame with geometries to determine distance to.
+            raster_profile: The raster profile of the raster in which the distances
+                            to the nearest geometry are determined.
+            max_distance: The maximum distance in the output array.
+
+        Returns:
+            A 2D numpy array with the the distance values inverted.
+    """
+
+    out_matrix = distance_computation(geodataframe, raster_profile, maximum_distance)
+
+    minimum = np.min(out_matrix)
+    difference = maximum_distance - minimum
+    out_matrix = maximum_distance - out_matrix
+    out_matrix = out_matrix/difference
+
+    return out_matrix
+
+@beartype
+def calculate_logarithmic_proximity(geodataframe: gpd.GeoDataFrame, raster_profile: Union[profiles.Profile, dict], maximum_distance: Number) -> np.ndarray:
+
+    """ Logarithmically interpolates the distance values calculated by the distance_computation function between 0 and 1.
+        1 denots the value inside the polygon and 0 at the maximum distance.
+        If maximum_distance value is not provided, the program sets this value to the maximum value 
+        in the provided distance matrix.
+        Uses linear interpolation to calculate the distance from the polygon.
+
+        Args:
+            geodataframe: The GeoDataFrame with geometries to determine distance to.
+            raster_profile: The raster profile of the raster in which the distances
+                            to the nearest geometry are determined.
+            max_distance: The maximum distance in the output array.
 
         Returns:
             A 2D numpy array with the the distance values inverted.
     """
     
-    if (maximum_distance is None):
-        maximum_distance = np.max(distance_array)
-        out_matrix = distance_array
-    else :
-        out_matrix = np.where(distance_array>maximum_distance,maximum_distance,distance_array)
+    distance_array = distance_computation(geodataframe, raster_profile,maximum_distance)
 
+    modified_distance_array = np.where(distance_array==0.0,np.nan,distance_array)
+    out_matrix = np.log(modified_distance_array)
+
+    log_maximum = np.log(maximum_distance)
     minimum = np.min(distance_array)
-    difference = maximum_distance - minimum
-    out_matrix = maximum_distance - out_matrix
+    if(minimum != 0):
+        log_minimum = np.log(minimum)
+    else :
+        log_minimum = minimum
+    difference = log_maximum - log_minimum
+    out_matrix = log_maximum - out_matrix
     out_matrix = out_matrix/difference
+
+    out_matrix = np.nan_to_num(out_matrix,nan=log_maximum)
 
     return out_matrix
