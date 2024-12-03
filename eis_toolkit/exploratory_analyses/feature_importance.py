5dd43b72512612edc076137122fbd3f1e1a8dc1b
msorvoja <mika@gispo.fi>
2024-12-03 12:58:02 +0200

    Modify parameters, add checks, fix tests

--- eis_toolkit/exploratory_analyses/feature_importance.py
+++ eis_toolkit/exploratory_analyses/feature_importance.py
@@ -5,16 +5,20 @@ from beartype import beartype
 from beartype.typing import Optional, Sequence
 from sklearn.inspection import permutation_importance
 
-from eis_toolkit.exceptions import InvalidDatasetException, InvalidParameterValueException
+from eis_toolkit.exceptions import (
+    InvalidDatasetException,
+    InvalidParameterValueException,
+    NonMatchingParameterLengthsException,
+)
 
 
 @beartype
 def evaluate_feature_importance(
     model: sklearn.base.BaseEstimator,
-    x_test: np.ndarray,
-    y_test: np.ndarray,
+    X: np.ndarray,
+    y: np.ndarray,
     feature_names: Sequence[str],
-    n_repeats: int = 50,
+    n_repeats: int = 10,
     random_state: Optional[int] = None,
 ) -> tuple[pd.DataFrame, dict]:
     """
@@ -22,10 +26,10 @@ def evaluate_feature_importance(
 
     Args:
         model: A trained and fitted Sklearn model.
-        x_test: Testing feature data (X data need to be normalized / standardized).
-        y_test: Testing label data.
+        X: Feature data (X data need to be normalized / standardized).
+        y: Target labels.
         feature_names: Names of the feature columns.
-        n_repeats: Number of iteration used when calculate feature importance. Defaults to 50.
+        n_repeats: Number of iteration used when calculate feature importance. Defaults to 10.
         random_state: random state for repeatability of results. Optional parameter.
 
     Returns:
@@ -37,18 +41,24 @@ def evaluate_feature_importance(
         InvalidParameterValueException: Value for 'n_repeats' is not at least one.
     """
 
-    if x_test.size == 0:
+    if X.size == 0:
         raise InvalidDatasetException("Array 'x_test' is empty.")
 
-    if y_test.size == 0:
+    if y.size == 0:
         raise InvalidDatasetException("Array 'y_test' is empty.")
 
     if n_repeats < 1:
         raise InvalidParameterValueException("Value for 'n_repeats' is less than one.")
 
-    result = permutation_importance(model, x_test, y_test.ravel(), n_repeats=n_repeats, random_state=random_state)
+    if len(X) != len(y):
+        raise NonMatchingParameterLengthsException("Feature matrix X and target labels y must have the same length.")
 
-    feature_importance = pd.DataFrame({"Feature": feature_names, "Importance": result.importances_mean * 100})
+    if len(feature_names) != X.shape[1]:
+        raise InvalidParameterValueException("Number of feature names must match the number of input features.")
+
+    result = permutation_importance(model, X, y.ravel(), n_repeats=n_repeats, random_state=random_state)
+
+    feature_importance = pd.DataFrame({"Feature": feature_names, "Importance": result.importances_mean})
 
     feature_importance = feature_importance.sort_values(by="Importance", ascending=False)
 
